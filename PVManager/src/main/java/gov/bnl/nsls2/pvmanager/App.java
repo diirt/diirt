0290b29e1d795179cd0704854803abc955b53f56
Gabriele Carcassi <carcassi@bnl.gov>
2010-02-08 15:09:52 -0500

    Initial scheleton for the project

---
+++ PVManager/src/main/java/gov/bnl/nsls2/pvmanager/App.java
@@ -0,0 +1,13 @@
+package gov.bnl.nsls2.pvmanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
