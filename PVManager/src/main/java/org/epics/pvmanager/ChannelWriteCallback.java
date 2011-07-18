9d9b6f11b6fafdea273427d1cd601d5e002a95bc
Gabriele Carcassi <carcassi@bnl.gov>
2011-07-18 15:14:37 -0400

    Javadocs update.

--- PVManager/src/main/java/org/epics/pvmanager/ChannelWriteCallback.java
+++ PVManager/src/main/java/org/epics/pvmanager/ChannelWriteCallback.java
@@ -5,9 +5,17 @@
 package org.epics.pvmanager;
 
 /**
+ * Called by the ChannelHandler once a write is completed.
  *
  * @author carcassi
  */
 public interface ChannelWriteCallback {
+    
+    /**
+     * Called when a write is completed. If completed without error,
+     * the argument is null.
+     * 
+     * @param ex the exception if the write failed, null otherwise
+     */
     public void channelWritten(Exception ex);
 }
