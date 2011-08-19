7a180792be4e2b034bcb74a61a450119e6691f6c
Gabriele Carcassi <carcassi@bnl.gov>
2011-08-19 13:37:43 -0400

    Sim: adding different arguments with defaults, to match utility.pv.

--- PVManager/src/main/java/org/epics/pvmanager/sim/Noise.java
+++ PVManager/src/main/java/org/epics/pvmanager/sim/Noise.java
@@ -35,6 +35,20 @@ public class Noise extends SimFunction<VDouble> {
     public Noise() {
         this(-5.0, 5.0, 0.1);
     }
+    
+    /**
+     * Do not use: only provided to provide some sort of error message
+     * for people migrating from utility.pv.
+     * 
+     * @param min minimum value
+     * @param max maximum value
+     * @param step ignored
+     * @param interval interval between samples in seconds
+     */
+    public Noise(Double min, Double max, Double step, Double interval) {
+        super(interval, VDouble.class);
+        throw new IllegalArgumentException("Please rename to noise(" + min + ", " + max + ", " + interval + ")");
+    }
 
     /**
      * Creates a signal uniformly distributed between min and max, updating
