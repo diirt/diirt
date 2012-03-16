97cc7205d059170bdae391c1a65b992e3408dd74
Gabriele Carcassi <carcassi@bnl.gov>
2012-03-16 15:55:39 -0400

    ValueAxis: added another case.

--- graphene/src/test/java/org/epics/graphene/ValueAxisTest.java
+++ graphene/src/test/java/org/epics/graphene/ValueAxisTest.java
@@ -84,6 +84,13 @@ public class ValueAxisTest {
         assertAxisEquals(0.0, 10.0, new double[]{0.0, 2.0, 4.0, 6.0, 8.0, 10.0},
                 new String[]{"0", "2", "4", "6", "8", "10"}, axis);
     }
+    
+    @Test
+    public void testTicksForRange6() {
+        ValueAxis axis = ValueAxis.createAutoAxis(0.0, 10.0, 8);
+        assertAxisEquals(0.0, 10.0, new double[]{0.0, 2.0, 4.0, 6.0, 8.0, 10.0},
+                new String[]{"0", "2", "4", "6", "8", "10"}, axis);
+    }
 
     private void assertAxisEquals(double minValue, double maxValue, double[] tickValues, String[] tickLabels, org.epics.graphene.ValueAxis axis) {
         assertEquals(minValue, axis.getMinValue(), 0.000001);
