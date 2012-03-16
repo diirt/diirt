6c17b7574029a20fbd82467e1ca447334fe4cdfb
Gabriele Carcassi <carcassi@bnl.gov>
2012-03-16 15:52:40 -0400

    ValueAxis: added another case.

--- graphene/src/main/java/org/epics/graphene/ValueAxis.java
+++ graphene/src/main/java/org/epics/graphene/ValueAxis.java
@@ -44,16 +44,108 @@ public class ValueAxis {
         return createAutoAxis(minValue, maxValue, maxTicks, Double.MIN_VALUE);
     }
     
-    private static final DecimalFormat format = new DecimalFormat("0.###");
+    private static final DecimalFormat defaultFormat = new DecimalFormat("0.###");
+    
+    private static final DecimalFormat decimal0 = new DecimalFormat("0");
+    private static final DecimalFormat decimal1 = new DecimalFormat("0.0");
+    private static final DecimalFormat decimal2 = new DecimalFormat("0.00");
     
     public static ValueAxis createAutoAxis(double minValue, double maxValue, int maxTicks, double minIncrement) {
-        double[] ticks = RangeUtil.ticksForRange(minValue, maxValue, maxTicks);
+        double increment = incrementForRange(minValue, maxValue, maxTicks, minIncrement);
+        double[] ticks = createTicks(minValue, maxValue, increment);
+        int rangeOrder = (int) orderOfMagnitude(minValue, maxValue);
+        int incrementOrder = (int) orderOfMagnitude(increment);
+        DecimalFormat format = defaultFormat;
+        if (incrementOrder == -1) {
+            format = decimal1;
+        }
+        
         String[] labels = new String[ticks.length];
         for (int i = 0; i < ticks.length; i++) {
             double value = ticks[i];
             labels[i] = format.format(value);
         }
         return new ValueAxis(minValue, maxValue, ticks, labels);
+    }
+    
+    static double orderOfMagnitude(double value) {
+        return Math.floor(Math.log10(value));
+    }
+    
+    static double orderOfMagnitude(double min, double max) {
+        return orderOfMagnitude(Math.max(Math.abs(max), Math.abs(min)));
+    }
+    
+    static double incrementForRange(double min, double max, int nTicks, double minIncrement) {
+        double magnitude = Math.pow(10.0, orderOfMagnitude(min, max));
+        if (magnitude < minIncrement) {
+            return Double.NaN;
+        }
+        int ticks = countTicks(min, max, magnitude);
+        if (ticks > nTicks) {
+            if (ticks / 2 < nTicks) {
+                int newTicks = countTicks(min, max, magnitude * 5);
+                if (newTicks > 2 && newTicks <= nTicks) {
+                    return magnitude * 5;
+                }
+            }
+            
+            if (ticks / 5 < nTicks) {
+                int newTicks = countTicks(min, max, magnitude * 2);
+                if (newTicks > 2 && newTicks <= nTicks) {
+                    return magnitude * 2;
+                }
+            }
+            
+            return Double.NaN;
+        } else {
+            double increment = magnitude;
+            // Refine if there is still space to refine
+            while (countTicks(min, max, increment / 2) <= nTicks) {
+                if (increment / 10 >= minIncrement && countTicks(min, max, increment / 10) <= nTicks) {
+                    increment /= 10;
+                } else if (increment / 5 >= minIncrement && countTicks(min, max, increment / 5) <= nTicks) {
+                    return increment / 5;
+                } else if(increment / 2 >= minIncrement) {
+                    return increment / 2;
+                } else {
+                    return increment;
+                }
+            }
+            return increment;
+        }
+    }
+    
+    /**
+     * Determines how many ticks would there be in that range using that increment.
+     * 
+     * @param min value range start
+     * @param max value range end
+     * @param increment space between ticks
+     * @return number of ticks in the range
+     */
+    static int countTicks(double min, double max, double increment) {
+        int start = (int) Math.ceil(min / increment);
+        int end = (int) Math.floor(max / increment);
+        return end - start + 1;
+    }
+    
+    /**
+     * Create values for the axis tick given the range and the increment.
+     * 
+     * @param min value range start
+     * @param max value range end
+     * @param increment space between ticks
+     * @return values for the ticks
+     */
+    static double[] createTicks(double min, double max, double increment) {
+        int start = (int) Math.ceil(min / increment);
+        int end = (int) Math.floor(max / increment);
+        double[] ticks = new double[end-start+1];
+        for (int i = 0; i < ticks.length; i++) {
+            ticks[i] = (i + start) * increment;
+        }
+        return ticks;
     }
     
 }
--- graphene/src/test/java/org/epics/graphene/ValueAxisTest.java
+++ graphene/src/test/java/org/epics/graphene/ValueAxisTest.java
@@ -66,6 +66,14 @@ public class ValueAxisTest {
         assertAxisEquals(0.0, 10.0, new double[]{0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0},
                 new String[]{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}, axis);
     }
+    
+    @Test
+    public void testTicksForRange3() {
+        ValueAxis axis = ValueAxis.createAutoAxis(0.0, 10.0, 21);
+        assertAxisEquals(0.0, 10.0, new double[]{0.0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0},
+                new String[]{"0.0", "0.5", "1.0", "1.5", "2.0", "2.5", "3.0", "3.5", "4.0", "4.5",
+                    "5.0", "5.5", "6.0", "6.5", "7.0", "7.5", "8.0", "8.5", "9.0", "9.5", "10.0"}, axis);
+    }
 
     private void assertAxisEquals(double minValue, double maxValue, double[] tickValues, String[] tickLabels, org.epics.graphene.ValueAxis axis) {
         assertEquals(minValue, axis.getMinValue(), 0.000001);
