ae0d8fbdd81c51895bb2e6ee4692559658d4ba03
Gabriele Carcassi <carcassi@bnl.gov>
2012-04-18 16:15:33 -0400

    Adding dataset conversion for double array and for single array rescaled.

--- graphene/src/main/java/org/epics/graphene/Arrays.java
+++ graphene/src/main/java/org/epics/graphene/Arrays.java
@@ -51,5 +51,94 @@ public class Arrays {
             }
         };
     }
+
+    public static OrderedDataset2D lineData(final double[] data, final double xInitialOffset, final double xIncrementSize) {
+        return new OrderedDataset2D() {
+            
+            private final double[] minMax = NumberUtil.minMax(data);
+
+            @Override
+            public double getXValue(int index) {
+                return xInitialOffset + xIncrementSize *index;
+            }
+
+            @Override
+            public double getYValue(int index) {
+                return data[index];
+            }
+
+            @Override
+            public double getXMinValue() {
+                return getXValue(0);
+            }
+
+            @Override
+            public double getXMaxValue() {
+                return getXValue(data.length);
+            }
+
+            @Override
+            public double getYMinValue() {
+                return minMax[0];
+            }
+
+            @Override
+            public double getYMaxValue() {
+                return minMax[1];
+            }
+
+            @Override
+            public int getCount() {
+                return data.length;
+            }
+        };
+    }
+
+    public static OrderedDataset2D lineData(final double[] x, final double[] y) {
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("Arrays length don't match: " + x.length + " - " + y.length);
+        }
+        
+        return new OrderedDataset2D() {
+            
+            private final double[] xMinMax = NumberUtil.minMax(x);
+            private final double[] yMinMax = NumberUtil.minMax(y);
+
+            @Override
+            public double getXValue(int index) {
+                return x[index];
+            }
+
+            @Override
+            public double getYValue(int index) {
+                return y[index];
+            }
+
+            @Override
+            public double getXMinValue() {
+                return xMinMax[0];
+            }
+
+            @Override
+            public double getXMaxValue() {
+                return xMinMax[1];
+            }
+
+            @Override
+            public double getYMinValue() {
+                return yMinMax[0];
+            }
+
+            @Override
+            public double getYMaxValue() {
+                return yMinMax[1];
+            }
+
+            @Override
+            public int getCount() {
+                return x.length;
+            }
+        };
+    }
     
 }
---
+++ graphene/src/test/java/org/epics/graphene/ArraysTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2011 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.graphene;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author carcassi
+ */
+public class ArraysTest {
+    
+    public ArraysTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Test
+    public void testArrayLineData() {
+        OrderedDataset2D dataset = Arrays.lineData(new double[] {1, 2, 3});
+        assertEquals(3, dataset.getCount());
+        assertEquals(0.0, dataset.getXValue(0), 0.001);
+        assertEquals(1.0, dataset.getXValue(1), 0.001);
+        assertEquals(2.0, dataset.getXValue(2), 0.001);
+        assertEquals(1.0, dataset.getYValue(0), 0.001);
+        assertEquals(2.0, dataset.getYValue(1), 0.001);
+        assertEquals(3.0, dataset.getYValue(2), 0.001);
+    }
+
+    @Test
+    public void testArrayScaledLineData() {
+        OrderedDataset2D dataset = Arrays.lineData(new double[] {1, 2, 3}, 10, 5);
+        assertEquals(3, dataset.getCount());
+        assertEquals(10.0, dataset.getXValue(0), 0.001);
+        assertEquals(15.0, dataset.getXValue(1), 0.001);
+        assertEquals(20.0, dataset.getXValue(2), 0.001);
+        assertEquals(1.0, dataset.getYValue(0), 0.001);
+        assertEquals(2.0, dataset.getYValue(1), 0.001);
+        assertEquals(3.0, dataset.getYValue(2), 0.001);
+    }
+
+    @Test
+    public void testDoubleArrayLineData() {
+        OrderedDataset2D dataset = Arrays.lineData(new double[] {1, 2, 3}, new double[] {3,7,5});
+        assertEquals(3, dataset.getCount());
+        assertEquals(1.0, dataset.getXValue(0), 0.001);
+        assertEquals(2.0, dataset.getXValue(1), 0.001);
+        assertEquals(3.0, dataset.getXValue(2), 0.001);
+        assertEquals(3.0, dataset.getYValue(0), 0.001);
+        assertEquals(7.0, dataset.getYValue(1), 0.001);
+        assertEquals(5.0, dataset.getYValue(2), 0.001);
+    }
+}
