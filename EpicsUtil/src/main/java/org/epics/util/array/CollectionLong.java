37e7f6201a42e0f2dd79c015b4c22bc8a1015f3c
Gabriele Carcassi <carcassi@bnl.gov>
2012-04-24 12:19:20 -0700

    Added prototype for numeric collections.

---
+++ EpicsUtil/src/main/java/org/epics/util/array/CollectionLong.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2011 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.util.array;
+
+/**
+ *
+ * @author carcassi
+ */
+public interface CollectionLong extends CollectionNumber {
+
+    IteratorLong iterator();
+    
+}
---
+++ EpicsUtil/src/main/java/org/epics/util/array/CollectionNumber.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright 2011 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.util.array;
+
+/**
+ *
+ * @author carcassi
+ */
+public interface CollectionNumber {
+    IteratorNumber iterator();
+    int size();
+}
