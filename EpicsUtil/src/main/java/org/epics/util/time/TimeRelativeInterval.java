784f1064fa172c2e8526b17a3448b4584a5c7d6b
Gabriele Carcassi <carcassi@bnl.gov>
2012-05-08 16:06:35 -0400

    First cut at the relative time interval.

---
+++ EpicsUtil/src/main/java/org/epics/util/time/TimeRelativeInterval.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.util.time;
+
+/**
+ *
+ * @author carcassi
+ */
+public class TimeRelativeInterval {
+    
+    private final Object start;
+    private final Object end;
+    
+    private TimeRelativeInterval(Object start, Object end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public static TimeRelativeInterval of(TimeStamp start, TimeStamp end) {
+        return new TimeRelativeInterval(start, end);
+    }
+    
+    public boolean isIntervalAbsolute() {
+        return isStartAbsolute() && isEndAbsolute();
+    }
+    
+    public boolean isStartAbsolute() {
+        return start instanceof TimeStamp;
+    }
+    
+    public boolean isEndAbsolute() {
+        return end instanceof TimeStamp;
+    }
+
+    public Object getStart() {
+        return start;
+    }
+
+    public Object getEnd() {
+        return end;
+    }
+
+    public TimeStamp getAbsoluteStart() {
+        return (TimeStamp) start;
+    }
+    
+    public TimeStamp getAbsoluteEnd() {
+        return (TimeStamp) end;
+    }
+    
+    public TimeDuration getRelativeStart() {
+        return (TimeDuration) start;
+    }
+    
+    public TimeDuration getRelativeEnd() {
+        return (TimeDuration) end;
+    }
+    
+    public TimeInterval toAbsoluteInterval(TimeStamp reference) {
+        TimeStamp absoluteStart;
+        if (isStartAbsolute()) {
+            absoluteStart = getAbsoluteStart();
+        } else {
+            absoluteStart = reference.plus(getRelativeStart());
+        }
+        TimeStamp absoluteEnd;
+        if (isEndAbsolute()) {
+            absoluteEnd = getAbsoluteEnd();
+        } else {
+            absoluteEnd = reference.plus(getRelativeEnd());
+        }
+        return TimeInterval.between(absoluteStart, absoluteEnd);
+    }
+    
+    
+}
---
+++ EpicsUtil/src/test/java/org/epics/util/time/TimeRelativeIntervalTest.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.util.time;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author carcassi
+ */
+public class TimeRelativeIntervalTest {
+
+    public TimeRelativeIntervalTest() {
+    }
+
+    // Trasform to absolute/relative?
+    // create aa/ar/ra/rr
+    @Test
+    public void of1() {
+        TimeRelativeInterval interval = TimeRelativeInterval.of(TimeStamp.of(0, 0), TimeStamp.of(3600, 0));
+        assertThat(interval.isIntervalAbsolute(), equalTo(true));
+        assertThat(interval.getAbsoluteStart(), equalTo(TimeStamp.of(0, 0)));
+        assertThat(interval.getAbsoluteEnd(), equalTo(TimeStamp.of(3600, 0)));
+//        assertThat(interval.toAbsoluteInterval(TimeStamp.now()), equalTo(TimeInterval.between(TimeStamp.of(0, 0), TimeStamp.of(3600, 0))));
+    }
+}
