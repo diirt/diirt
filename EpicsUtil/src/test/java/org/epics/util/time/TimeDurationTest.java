0e7992bdf60055a715897c54955a1fff1a773dd7
Gabriele Carcassi <carcassi@bnl.gov>
2012-05-08 10:46:47 -0400

    Reviewing tests.

---
+++ EpicsUtil/src/test/java/org/epics/util/time/TimeDurationTest.java
@@ -0,0 +1,45 @@
+package org.epics.util.time;
+
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+
+
+import org.epics.util.time.*;
+import static org.epics.util.time.TimeDuration.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author carcassi
+ */
+public class TimeDurationTest {
+
+    public TimeDurationTest() {
+    }
+    
+    // TimeDuration
+    // - test that factory method create expected sec/nano, including negative
+    // - test equality between different factory methods, including negative
+    // - test plus/minus with equlity, including carry and negative
+
+    
+    // Test factory methods
+
+    @Test
+    public void nanos1() {
+        TimeDuration duration = TimeDuration.nanos(100L);
+        assertThat(duration.getNanoSec(), equalTo(100));
+        assertThat(duration.getSec(), equalTo(0L));
+    }
+
+    @Test
+    public void nanos2() {
+        TimeDuration duration = TimeDuration.nanos(1234567890L);
+        assertThat(duration.getNanoSec(), equalTo(234567890));
+        assertThat(duration.getSec(), equalTo(1L));
+    }
+}
