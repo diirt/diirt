e18e1fe5168c3113282f9a9c651144dd012f851f
Gabriele Carcassi <carcassi@bnl.gov>
2012-05-21 11:21:43 -0400

    Adding VNumberArray as a superclass to all numeric arrays. It uses ListNumber as a superclass for the numeric data.

--- PVManager/src/main/java/org/epics/pvmanager/data/IVDoubleArray.java
+++ PVManager/src/main/java/org/epics/pvmanager/data/IVDoubleArray.java
@@ -6,6 +6,8 @@ package org.epics.pvmanager.data;
 
 import java.text.NumberFormat;
 import java.util.List;
+import org.epics.util.array.ArrayDouble;
+import org.epics.util.array.ListDouble;
 import org.epics.util.time.Timestamp;
 
 /**
@@ -38,6 +40,11 @@ class IVDoubleArray extends IVNumeric implements VDoubleArray {
     @Override
     public List<Integer> getSizes() {
         return sizes;
+    }
+
+    @Override
+    public ListDouble getListNumber() {
+        return new ArrayDouble(array);
     }
 
 }
