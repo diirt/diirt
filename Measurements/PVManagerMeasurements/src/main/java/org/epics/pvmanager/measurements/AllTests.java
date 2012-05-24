8da72ae39f77116311457ac010ba9e5c1d139bbe
Gabriele Carcassi <carcassi@bnl.gov>
2012-05-24 11:33:04 -0400

    Testing connection speed using channel finder.

--- Measurements/PVManagerMeasurements/src/main/java/org/epics/pvmanager/measurements/AllTests.java
+++ Measurements/PVManagerMeasurements/src/main/java/org/epics/pvmanager/measurements/AllTests.java
@@ -4,6 +4,13 @@
  */
 package org.epics.pvmanager.measurements;
 
+import gov.bnl.channelfinder.api.Channel;
+import gov.bnl.channelfinder.api.ChannelFinderClient;
+import gov.bnl.channelfinder.api.ChannelFinderClientImpl.CFCBuilder;
+import gov.bnl.channelfinder.api.ChannelUtil;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.epics.pvmanager.PVManager;
 import org.epics.pvmanager.jca.JCADataSource;
 
@@ -14,6 +21,11 @@ import org.epics.pvmanager.jca.JCADataSource;
 public class AllTests {
     public static void main(String[] args) throws Exception {
         PVManager.setDefaultDataSource(new JCADataSource());
+        ChannelFinderClient cf = CFCBuilder.serviceURL("http://channelfinder.nsls2.bnl.gov").create();
+        Collection<Channel> queriedChannels = cf.find("Tags=aphla.sys.SR");
+        Collection<String> channelNames = ChannelUtil.getChannelNames(queriedChannels);
+        System.out.println(channelNames.size());
+        
         String[] channels20 = new String[] {"counter1", "counter2", "counter3", "counter4",
             "counter5", "counter6", "counter7", "counter8", "counter9", "counter10",
             "counter11", "counter12", "counter13", "counter14",
@@ -22,21 +34,30 @@ public class AllTests {
             "counter5", "counter6", "counter7", "counter8", "counter9", "counter10",
             "counter11", "counter12", "counter13", "counter14",
             "counter15", "counter16", "counter17", "counter18", "counter19", "counter20"};
-        String[] channels = channels20;
+        List<String> channelList = new ArrayList<String>();
+        String[] channels = channelNames.toArray(new String[channelNames.size()]);
         //ConnectionDelay.main(new String[] {"counter1"});
 //        ConnectionDelay.main(new String[] {"counter1", "counter2", "counter3", "counter4",
 //            "counter5", "counter6", "counter7", "counter8", "counter9", "counter10"});
-        JCAMassConnectionDelay.main(channels);
+        //JCAMassConnectionDelay.main(channels);
         Thread.sleep(1000);
-        ConsecutiveConnectionDelay.main(channels);
-        Thread.sleep(1000);
-        JCAMassConnectionDelay.main(channels);
-        Thread.sleep(1000);
-        ConsecutiveConnectionDelay.main(channels);
+        ListConnectionDelay.main(channels);
         Thread.sleep(1000);
         JCAMassConnectionDelay.main(channels);
         Thread.sleep(1000);
-        ConsecutiveConnectionDelay.main(channels);
+        ListConnectionDelay.main(channels);
+        Thread.sleep(1000);
+        JCAMassConnectionDelay.main(channels);
+        Thread.sleep(1000);
+        ListConnectionDelay.main(channels);
+        Thread.sleep(1000);
+        JCAMassConnectionDelay.main(channels);
+        Thread.sleep(1000);
+        ListConnectionDelay.main(channels);
+        Thread.sleep(1000);
+        JCAMassConnectionDelay.main(channels);
+        Thread.sleep(1000);
+        ListConnectionDelay.main(channels);
         //PVManager.getDefaultDataSource().close();
         System.exit(0);
     }
