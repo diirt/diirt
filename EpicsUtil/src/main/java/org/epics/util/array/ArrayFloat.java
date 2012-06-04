a3261da3c45ba90e244f9c2793091b3116cf0eeb
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-04 15:08:19 -0400

    Implementing serialization for ArrayFloat.

--- EpicsUtil/src/main/java/org/epics/util/array/ArrayFloat.java
+++ EpicsUtil/src/main/java/org/epics/util/array/ArrayFloat.java
@@ -4,6 +4,7 @@
  */
 package org.epics.util.array;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
 /**
@@ -11,7 +12,9 @@ import java.util.Arrays;
  *
  * @author Gabriele Carcassi
  */
-public final class ArrayFloat extends ListFloat {
+public final class ArrayFloat extends ListFloat implements Serializable {
+    
+    private static final long serialVersionUID = 7493025761455302918L;
     
     private final float[] array;
     private final boolean readOnly;
