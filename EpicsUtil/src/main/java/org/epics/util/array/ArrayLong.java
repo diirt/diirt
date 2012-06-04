8b75ccc3319773c5f20ff324bf057153f9d7e3e2
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-04 15:11:13 -0400

    Implementing serialization for ArrayLong.

--- EpicsUtil/src/main/java/org/epics/util/array/ArrayLong.java
+++ EpicsUtil/src/main/java/org/epics/util/array/ArrayLong.java
@@ -4,6 +4,7 @@
  */
 package org.epics.util.array;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
 /**
@@ -11,7 +12,9 @@ import java.util.Arrays;
  *
  * @author Gabriele Carcassi
  */
-public final class ArrayLong extends ListLong {
+public final class ArrayLong extends ListLong implements Serializable {
+    
+    private static final long serialVersionUID = 7493025761455302920L;
     
     private final long[] array;
     private final boolean readOnly;
