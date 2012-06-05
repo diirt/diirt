cd0f8c44d0b79da13cd8e6c7c1efc4eac1488ddb
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-05 15:37:21 -0400

    Javadoc update.

--- PVManager/src/main/java/org/epics/pvmanager/DataSourceTypeAdapterSet.java
+++ PVManager/src/main/java/org/epics/pvmanager/DataSourceTypeAdapterSet.java
@@ -11,9 +11,19 @@ package org.epics.pvmanager;
 import java.util.Collection;
 
 /**
+ * A set of type adapters. This optional class is provided to help
+ * create a more flexible type support in a datasource, so that support
+ * for individual types is done through runtime configuration.
  *
  * @author carcassi
  */
 public interface DataSourceTypeAdapterSet {
+    
+    /**
+     * Returns a collation of adapters. The collection must be
+     * immutable.
+     * 
+     * @return a collection; not null
+     */
     Collection<? extends DataSourceTypeAdapter<?, ?>> getAdapters();
 }
