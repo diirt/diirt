e0fd17338fd9dfc2ccf764f98186a3a9c0b1b4d5
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-06 16:55:43 -0400

    Dataset1DUpdate can accept ListNumber.

--- graphene/src/main/java/org/epics/graphene/Dataset1DUpdate.java
+++ graphene/src/main/java/org/epics/graphene/Dataset1DUpdate.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import org.epics.util.array.IteratorDouble;
 import org.epics.util.array.IteratorNumber;
+import org.epics.util.array.ListNumber;
 
 /**
  *
@@ -18,13 +19,17 @@ public class Dataset1DUpdate {
     protected Collection<IteratorNumber> newData = new ArrayList<IteratorNumber>();
     protected boolean clear;
     
-    public Dataset1DUpdate addData(IteratorDouble data) {
+    public Dataset1DUpdate addData(IteratorNumber data) {
         newData.add(data);
         return this;
     }
     
     public Dataset1DUpdate addData(double[] data) {
         return addData(Iterators.arrayIterator(data));
+    }
+    
+    public Dataset1DUpdate addData(ListNumber data) {
+        return addData(data.iterator());
     }
     
     public Dataset1DUpdate addData(double data) {
