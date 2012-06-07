91441cd3b61655deb85fb3d55d24799a119e31dc
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-07 12:06:45 -0400

    Wrong call.

--- PVManager/src/main/java/org/epics/pvmanager/data/ValueFactory.java
+++ PVManager/src/main/java/org/epics/pvmanager/data/ValueFactory.java
@@ -70,7 +70,7 @@ public class ValueFactory {
                 time.getTimestamp(), time.getTimeUserTag(), time.isTimeValid(),
                 display.getLowerDisplayLimit(), display.getLowerCtrlLimit(), display.getLowerAlarmLimit(), display.getLowerWarningLimit(),
                 display.getUnits(), display.getFormat(),
-                display.getLowerWarningLimit(), display.getUpperAlarmLimit(), display.getUpperCtrlLimit(), display.getUpperDisplayLimit());
+                display.getUpperWarningLimit(), display.getUpperAlarmLimit(), display.getUpperCtrlLimit(), display.getUpperDisplayLimit());
     }
     
     /**
