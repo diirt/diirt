a9e9782eec7e8a0836104a8d054551922e3b8e6a
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-08 13:35:13 -0400

    Improving StopWatch to use ListNumber.

--- graphene/src/test/java/org/epics/graphene/profile/StopWatch.java
+++ graphene/src/test/java/org/epics/graphene/profile/StopWatch.java
@@ -4,6 +4,11 @@
  */
 package org.epics.graphene.profile;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+import org.epics.util.array.ArrayLong;
+import org.epics.util.array.ListLong;
+
 /**
  *
  * @author carcassi
@@ -34,11 +39,33 @@ public class StopWatch {
         return average / nAttempts;
     }
     
+    public long getTotalMs() {
+        long total = 0;
+        for (int i = 0; i < nAttempts; i++) {
+            total += timings[i] / 1000000.0;
+        }
+        return total;
+    }
+    
     public double[] getData() {
         double[] data = new double[nAttempts];
         for (int i = 0; i < nAttempts; i++) {
             data[i] += timings[i] / 1000000.0;
         }
         return data;
+    }
+    
+    public ListLong getNanoTimings() {
+        return new ArrayLong(Arrays.copyOfRange(timings, 0, nAttempts));
+    }
+    
+    public ListLong getNanoAverages(int start) {
+        long[] averages = new long[nAttempts - start];
+        BigInteger total = BigInteger.ZERO;
+        for (int i = 0; i < averages.length; i++) {
+            total = total.add(BigInteger.valueOf(timings[i + start]));
+            averages[i] = total.divide(BigInteger.valueOf(i+1)).longValue();
+        }
+        return new ArrayLong(averages);
     }
 }
