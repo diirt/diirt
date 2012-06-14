48ed4d0ef47d9bda3807ecf055b148f7f15ef05b
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-14 12:05:44 -0400

    Adding toString to ListDouble.

--- EpicsUtil/src/test/java/org/epics/util/array/ListDoubleTest.java
+++ EpicsUtil/src/test/java/org/epics/util/array/ListDoubleTest.java
@@ -84,4 +84,21 @@ public class ListDoubleTest {
         assertThat(coll.hashCode(), equalTo(Arrays.hashCode(new double[] {0,1,2,3,4,5,6,7,8,9})));
     }
     
+    @Test
+    public void toString1() {
+        ListDouble coll = new ListDouble() {
+
+            @Override
+            public int size() {
+                return 10;
+            }
+
+            @Override
+            public double getDouble(int index) {
+                return index;
+            }
+        };
+        assertThat(coll.toString(), equalTo("[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]"));
+    }
+    
 }
