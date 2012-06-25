75cc09b55028609ef6d8265da1db80b268ff1525
Gabriele Carcassi <carcassi@bnl.gov>
2012-06-25 12:18:27 -0400

    Javadoc update.

--- PVManager/src/main/java/org/epics/pvmanager/jca/JCATypeSupport.java
+++ PVManager/src/main/java/org/epics/pvmanager/jca/JCATypeSupport.java
@@ -13,6 +13,14 @@ import org.epics.pvmanager.DataSourceTypeSupport;
 import org.epics.pvmanager.ValueCache;
 
 /**
+ * 
+ * Given a set of {@link JCATypeAdapter} prepares type support for the 
+ * JCA data source.
+ * <p>
+ * Given that the datasource uses multiplexed channel, the type support need to
+ * be consistent. This mean that all the type that will match the same channel,
+ * will need to request the same metadata (or some requests will be given
+ * the wrong value to convert).
  *
  * @author carcassi
  */
@@ -20,6 +28,11 @@ public class JCATypeSupport extends DataSourceTypeSupport {
     
     private final JCATypeAdapterSet adapters;
 
+    /**
+     * A new type support for the jca type support.
+     * 
+     * @param adapters a set of jca adapters
+     */
     public JCATypeSupport(JCATypeAdapterSet adapters) {
         this.adapters = adapters;
     }
