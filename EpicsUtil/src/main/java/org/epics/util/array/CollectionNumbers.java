7ea31ba558e14b03ca34644ed4a6023a11ddfb94
Gabriele Carcassi <carcassi@bnl.gov>
2012-08-08 16:54:05 -0400

    Adding array copies for all types

--- EpicsUtil/src/main/java/org/epics/util/array/CollectionNumbers.java
+++ EpicsUtil/src/main/java/org/epics/util/array/CollectionNumbers.java
@@ -25,12 +25,97 @@ public class CollectionNumbers {
      * @param coll the collection
      * @return the array
      */
+    public static float[] floatArrayCopyOf(CollectionNumber coll) {
+        float[] data = new float[coll.size()];
+        IteratorNumber iter = coll.iterator();
+        int index = 0;
+        while (iter.hasNext()) {
+            data[index] = iter.nextFloat();
+            index++;
+        }
+        return data;
+    }
+    
+    /**
+     * Copies the content of the collection to an array.
+     * 
+     * @param coll the collection
+     * @return the array
+     */
     public static double[] doubleArrayCopyOf(CollectionNumber coll) {
         double[] data = new double[coll.size()];
         IteratorNumber iter = coll.iterator();
         int index = 0;
         while (iter.hasNext()) {
             data[index] = iter.nextDouble();
+            index++;
+        }
+        return data;
+    }
+    
+    /**
+     * Copies the content of the collection to an array.
+     * 
+     * @param coll the collection
+     * @return the array
+     */
+    public static byte[] byteArrayCopyOf(CollectionNumber coll) {
+        byte[] data = new byte[coll.size()];
+        IteratorNumber iter = coll.iterator();
+        int index = 0;
+        while (iter.hasNext()) {
+            data[index] = iter.nextByte();
+            index++;
+        }
+        return data;
+    }
+    
+    /**
+     * Copies the content of the collection to an array.
+     * 
+     * @param coll the collection
+     * @return the array
+     */
+    public static short[] shortArrayCopyOf(CollectionNumber coll) {
+        short[] data = new short[coll.size()];
+        IteratorNumber iter = coll.iterator();
+        int index = 0;
+        while (iter.hasNext()) {
+            data[index] = iter.nextShort();
+            index++;
+        }
+        return data;
+    }
+    
+    /**
+     * Copies the content of the collection to an array.
+     * 
+     * @param coll the collection
+     * @return the array
+     */
+    public static int[] intArrayCopyOf(CollectionNumber coll) {
+        int[] data = new int[coll.size()];
+        IteratorNumber iter = coll.iterator();
+        int index = 0;
+        while (iter.hasNext()) {
+            data[index] = iter.nextInt();
+            index++;
+        }
+        return data;
+    }
+    
+    /**
+     * Copies the content of the collection to an array.
+     * 
+     * @param coll the collection
+     * @return the array
+     */
+    public static long[] longArrayCopyOf(CollectionNumber coll) {
+        long[] data = new long[coll.size()];
+        IteratorNumber iter = coll.iterator();
+        int index = 0;
+        while (iter.hasNext()) {
+            data[index] = iter.nextLong();
             index++;
         }
         return data;
