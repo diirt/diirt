f77e82d9ec01dfa86e832fb39e7a370d8a8270cb
Gabriele Carcassi <carcassi@bnl.gov>
2012-08-16 13:56:04 -0400

    Adding vTypes for a list of channels that produce vTypes.

--- PVManager/src/main/java/org/epics/pvmanager/data/ExpressionLanguage.java
+++ PVManager/src/main/java/org/epics/pvmanager/data/ExpressionLanguage.java
@@ -97,6 +97,15 @@ public class ExpressionLanguage {
     public static ChannelExpression<VType, Object> vType(String name) {
         return channel(name, VType.class, Object.class);
     }
+    /**
+     * A list of channels with the given names that return any of the value type.
+     *
+     * @param names the channel names; can't be null
+     * @return a list of expressions representing the channels
+     */
+    public static ChannelExpressionList<VType, Object> vTypes(List<String> name) {
+        return channels(name, VType.class, Object.class);
+    }
 
     /**
      * A channel with the given name of type VNumber.
