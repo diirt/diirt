735c2c097375cf16c5a2f6c44ced4b3628727923
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-04 14:38:21 -0400

    Fixing copyright notice.

--- PVManager/src/main/java/org/epics/pvmanager/pva/adapters/PVFieldToVEnum.java
+++ PVManager/src/main/java/org/epics/pvmanager/pva/adapters/PVFieldToVEnum.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
 package org.epics.pvmanager.pva.adapters;
 
 import java.util.Arrays;
