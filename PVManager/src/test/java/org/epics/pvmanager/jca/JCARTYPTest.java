8d811f95b43e715d407077c709f28aabc2187d6b
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-10 16:18:19 -0400

    JCAMessagePayload filles in time and alarm if the string monitor value does not have them.

---
+++ PVManager/src/test/java/org/epics/pvmanager/jca/JCARTYPTest.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.epics.pvmanager.jca;
+
+import gov.aps.jca.CAStatus;
+import gov.aps.jca.Channel;
+import gov.aps.jca.dbr.DBR;
+import gov.aps.jca.dbr.DBR_String;
+import gov.aps.jca.dbr.DBR_TIME_String;
+import gov.aps.jca.dbr.Severity;
+import gov.aps.jca.dbr.Status;
+import gov.aps.jca.event.MonitorEvent;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.epics.pvmanager.ExpressionLanguage.*;
+import org.epics.pvmanager.expression.DesiredRateExpression;
+import org.epics.pvmanager.expression.DesiredRateReadWriteExpression;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+/**
+ *
+ * @author carcassi
+ */
+public class JCARTYPTest {
+    
+    public JCARTYPTest() {
+    }
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+    }
+    
+    @Mock Channel channel;
+    @Mock JCADataSource dataSource;
+
+    @Test
+    public void payloadConvert1() {
+        DBR_TIME_String value = new DBR_TIME_String(new String[]{"theValue"});
+        value.setSeverity(Severity.MINOR_ALARM);
+        value.setStatus(Status.LOW_ALARM);
+        MonitorEvent event = new MonitorEvent(channel, value, CAStatus.NORMAL);
+        JCAMessagePayload payload = new JCAMessagePayload(null, event);
+        assertThat(payload.getEvent().getDBR(), sameInstance((DBR) value));
+    }
+
+    @Test
+    public void payloadConvert2() {
+        DBR_String value = new DBR_String(new String[]{"theValue"});
+        MonitorEvent event = new MonitorEvent(channel, value, CAStatus.NORMAL);
+        JCAMessagePayload payload = new JCAMessagePayload(null, event);
+        assertThat(payload.getEvent().getDBR(), not(sameInstance((DBR) value)));
+        assertThat(payload.getEvent().getDBR(), instanceOf(DBR_TIME_String.class));
+    }
+}
