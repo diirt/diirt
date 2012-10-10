33996688163473a739ff4c40f6a40f9e995d303a
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-10 14:41:33 -0400

    Adding test to check for multiple connections.

---
+++ PVManager/src/test/java/org/epics/pvmanager/sim/BlackBoxTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.sim;
+
+import org.epics.pvmanager.*;
+import static org.epics.pvmanager.ExpressionLanguage.*;
+import static org.epics.util.time.TimeDuration.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author carcassi
+ */
+public class BlackBoxTest {
+
+    @Test
+    public void multipleRead1() throws Exception {
+        String channelName = "ramp(0,100,1,0.01)";
+        DataSource dataSource = new SimulationDataSource();
+        
+        PVReader<Object> pv1 = PVManager.read(channel(channelName)).from(dataSource).maxRate(ofHertz(50));
+        PVReader<Object> pv2 = PVManager.read(channel(channelName)).from(dataSource).maxRate(ofHertz(50));
+        Thread.sleep(50);
+        assertThat(pv1.getValue(), not(nullValue()));
+        assertThat(pv2.getValue(), not(nullValue()));
+        assertThat(pv1.isConnected(), equalTo(true));
+        assertThat(pv2.isConnected(), equalTo(true));
+        pv1.close();
+        pv2.close();
+    }
+    
+}
