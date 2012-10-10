59b967f3cb2f375814c8247282d37feec9d609e5
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-10 11:05:00 -0400

    Using new jca builder.

--- PVManager/src/test/java/org/epics/pvmanager/tests/SetupUtil.java
+++ PVManager/src/test/java/org/epics/pvmanager/tests/SetupUtil.java
@@ -7,6 +7,7 @@ package org.epics.pvmanager.tests;
 import org.epics.pvmanager.CompositeDataSource;
 import org.epics.pvmanager.PVManager;
 import org.epics.pvmanager.jca.JCADataSource;
+import org.epics.pvmanager.jca.JCADataSourceBuilder;
 import org.epics.pvmanager.sim.SimulationDataSource;
 import org.epics.pvmanager.util.Executors;
 
@@ -18,7 +19,7 @@ public class SetupUtil {
     public static void defaultCASetup() {
         CompositeDataSource dataSource = new CompositeDataSource();
         dataSource.putDataSource("sim", SimulationDataSource.simulatedData());
-        dataSource.putDataSource("ca", new JCADataSource());
+        dataSource.putDataSource("ca", new JCADataSourceBuilder().build());
         dataSource.setDefaultDataSource("ca");
         PVManager.setDefaultDataSource(dataSource);
     }
