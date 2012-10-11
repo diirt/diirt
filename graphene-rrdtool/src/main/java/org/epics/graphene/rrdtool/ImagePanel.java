9d6bfc1bde4caea1c16d88afbfad1395eab6f15c
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-11 15:30:08 -0400

    First simple ui to test reading.

---
+++ graphene-rrdtool/src/main/java/org/epics/graphene/rrdtool/ImagePanel.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2012 University of Michigan
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.graphene.rrdtool;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author carcassi
+ */
+public class ImagePanel extends JComponent {
+
+    private Image image;
+
+    public void setImage(Image image) {
+        this.image = image;
+        if (image != null) {
+            setSize(new Dimension(image.getWidth(this), image.getHeight(this)));
+            setPreferredSize(new Dimension(image.getWidth(this), image.getHeight(this)));
+            //setMaximumSize(new Dimension(image.getHeight(this), image.getWidth(this)));
+            //setMinimumSize(new Dimension(image.getHeight(this), image.getWidth(this)));
+        } else {
+            
+        }
+        revalidate();
+        repaint();
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        if (image != null) {
+            g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
+}
--- graphene-rrdtool/src/main/java/org/epics/graphene/rrdtool/RrdToolReader.java
+++ graphene-rrdtool/src/main/java/org/epics/graphene/rrdtool/RrdToolReader.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) 2012 University of Michigan
+ * All rights reserved. Use is subject to license terms.
+ */
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
