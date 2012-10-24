36c5b3a16661ed749583cb8b7edc352a58b37932
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-24 19:37:01 -0400

    Adding a class to represent the write parameter to subscribe to a channel.

---
+++ PVManager/src/main/java/org/epics/pvmanager/ChannelHandlerWriteSubscription.java
@@ -0,0 +1,62 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.epics.pvmanager;
+
+/**
+ * Groups all the parameters required to add a writer to a ChannelHandler.
+ *
+ * @author carcassi
+ */
+public class ChannelHandlerWriteSubscription {
+
+    public ChannelHandlerWriteSubscription(WriteCache<?> cache, ExceptionHandler handler, Function<Boolean> connectionCache, Collector<Boolean> connectionCollector) {
+        this.cache = cache;
+        this.handler = handler;
+        this.connectionCache = connectionCache;
+        this.connectionCollector = connectionCollector;
+    }
+    
+    private final WriteCache<?> cache;
+    private final ExceptionHandler handler;
+    private final Function<Boolean> connectionCache;
+    private final Collector<Boolean> connectionCollector;
+
+    /**
+     * The cache to get the value to write.
+     * 
+     * @return the write cache
+     */
+    public WriteCache<?> getCache() {
+        return cache;
+    }
+
+    /**
+     * The exception handler for connection/disconnection errors.
+     * 
+     * @return the exception handler
+     */
+    public ExceptionHandler getHandler() {
+        return handler;
+    }
+
+    /**
+     * The cache to hold the connection flag.
+     * 
+     * @return the cache
+     */
+    public Function<Boolean> getConnectionCache() {
+        return connectionCache;
+    }
+
+    /**
+     * The collector to notify when the connection changes.
+     * 
+     * @return the collector
+     */
+    public Collector<Boolean> getConnectionCollector() {
+        return connectionCollector;
+    }
+    
+}
