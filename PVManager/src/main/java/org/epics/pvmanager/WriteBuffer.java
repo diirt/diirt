82eaaa9761ff4bf5d4dd2ba098f3467eb7151012
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-10-24 19:11:22 -0400

    Starting to add the connection caches and collector in the write buffer.

--- PVManager/src/main/java/org/epics/pvmanager/WriteBuffer.java
+++ PVManager/src/main/java/org/epics/pvmanager/WriteBuffer.java
@@ -5,6 +5,7 @@
 package org.epics.pvmanager;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import org.epics.pvmanager.WriteCache;
 
@@ -16,9 +17,13 @@ import org.epics.pvmanager.WriteCache;
  */
 public class WriteBuffer {
     private final Map<String, WriteCache<?>> caches;
+    private final Collector<Boolean> connectionCollector;
+    private final Map<String, ValueCache<Boolean>> connectionCaches;
 
     WriteBuffer(Map<String, WriteCache<?>> caches) {
         this.caches = Collections.unmodifiableMap(caches);
+        this.connectionCaches = generateConnectionCaches();
+        this.connectionCollector = new ConnectionCollector(connectionCaches);
     }
     
     /**
@@ -28,6 +33,36 @@ public class WriteBuffer {
      */
     public Map<String, WriteCache<?>> getWriteCaches() {
         return caches;
+    }
+    
+    private Map<String, ValueCache<Boolean>> generateConnectionCaches() {
+        // TODO: should refactor with similar method in DataRecipe
+        Map<String, ValueCache<Boolean>> newCaches = new HashMap<String, ValueCache<Boolean>>();
+        for (String name : caches.keySet()) {
+            ValueCache<Boolean> cache = new ValueCache<Boolean>(Boolean.class);
+            cache.setValue(false);
+            newCaches.put(name, cache);
+        }
+        return newCaches;
+    }
+
+    /**
+     * Returns the collector with the connection value to be passed
+     * to the PVWriter.
+     * 
+     * @return the connection collector
+     */
+    public Collector<Boolean> getConnectionCollector() {
+        return connectionCollector;
+    }
+
+    /**
+     * The caches that will hold the connection flag.
+     * 
+     * @return a map from the pv name to the connection cache
+     */
+    public Map<String, ValueCache<Boolean>> getConnectionCaches() {
+        return connectionCaches;
     }
     
 }
