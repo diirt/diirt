b2d8f781dd41c27b2e5bf7ee8173543aba1a2a57
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-11-01 11:03:37 -0400

    Adding interface for converting labels to colors.

---
+++ graphene/src/main/java/org/epics/graphene/LabelColorScheme.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.epics.graphene;
+
+/**
+ *
+ * @author carcassi
+ */
+public interface LabelColorScheme {
+    public int getColor(String label);
+}
