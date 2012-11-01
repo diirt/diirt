aafab23b981125ab00700a3648ee514f03f609bd
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-11-01 09:41:13 -0400

    Adding utility to find min and max discarding NaNs.

---
+++ graphene/src/main/java/org/epics/graphene/MathIgnoreNaN.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.epics.graphene;
+
+/**
+ *
+ * @author carcassi
+ */
+public class MathIgnoreNaN {
+    public static double min(double a, double b) {
+        if (Double.isNaN(a)) {
+            return b;
+        }
+        
+        if (Double.isNaN(b)) {
+            return a;
+        }
+        
+        return Math.min(a, b);
+    }
+    
+    public static double max(double a, double b) {
+        if (Double.isNaN(a)) {
+            return b;
+        }
+        
+        if (Double.isNaN(b)) {
+            return a;
+        }
+        
+        return Math.max(a, b);
+    }
+}
