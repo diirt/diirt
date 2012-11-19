73c92df0f4c83829d1b84918b5e1b518a93c9cb2
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-11-19 08:58:12 -0500

    Javadoc update.

--- PVManager/src/main/java/org/epics/pvmanager/CacheCollector.java
+++ PVManager/src/main/java/org/epics/pvmanager/CacheCollector.java
@@ -2,10 +2,6 @@
  * Copyright (C) 2010-12 Brookhaven National Laboratory
  * All rights reserved. Use is subject to license terms.
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.epics.pvmanager;
 
 import java.util.ArrayList;
