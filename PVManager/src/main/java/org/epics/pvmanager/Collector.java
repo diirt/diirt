d433ca28af6f49de037b90d1ca9ad0d8e15ae538
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-11-28 17:24:04 -0500

    Javadoc update

--- PVManager/src/main/java/org/epics/pvmanager/Collector.java
+++ PVManager/src/main/java/org/epics/pvmanager/Collector.java
@@ -8,6 +8,8 @@ package org.epics.pvmanager;
  * A collector can be written from one thread and read from another and provides
  * the point where two subsystems and their rate can be decoupled.
  *
+ * @param <I> the type written in the collector
+ * @param <O> the type read from the collector
  * @author carcassi
  */
 public interface Collector<I, O> extends WriteFunction<I>, ReadFunction<O> {
