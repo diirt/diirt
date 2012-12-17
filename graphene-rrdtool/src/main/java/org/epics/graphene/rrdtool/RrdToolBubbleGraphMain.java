a8193b07534406e0b6fb2a4b0ae0a8e728cb82d0
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2012-12-17 14:40:14 -0500

    Using bubble graph to correlate the series.

---
+++ graphene-rrdtool/src/main/java/org/epics/graphene/rrdtool/RrdToolBubbleGraphMain.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2012 University of Michigan
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.graphene.rrdtool;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.imageio.ImageIO;
+import org.epics.graphene.Bubble2DGraphRenderer;
+import org.epics.graphene.Point3DWithLabelDataset;
+import org.epics.graphene.Point3DWithLabelDatasets;
+import org.epics.graphene.Scatter2DGraphRenderer;
+import org.epics.util.array.ListDouble;
+import org.epics.util.time.TimeDuration;
+import org.epics.util.time.Timestamp;
+import org.epics.util.time.TimestampFormat;
+
+/**
+ *
+ * @author carcassi
+ */
+public class RrdToolBubbleGraphMain {
+    
+    private static TimestampFormat format = new TimestampFormat("yyyyMMddHHmmss");
+    
+    public static void main(String[] args) throws Exception {
+        List<String> signals = new ArrayList<>();
+        Timestamp start = null;
+        Timestamp end = null;
+        String filename = null;
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.equals("-s")) {
+                i++;
+                start = format.parse(args[i]);
+            } else if (arg.equals("-e")) {
+                i++;
+                end = format.parse(args[i]);
+            } else if (arg.equals("-o")) {
+                i++;
+                filename = args[i];
+                if (!filename.endsWith(".png")) {
+                    filename = filename + ".png";
+                }
+            } else  {
+                signals.add(arg);
+            }
+        }
+        List<TimeSeries> data = RrdToolDB.fetchData(signals, start, end);
+        Map<String, TimeSeries> series = new HashMap<>();
+        for (int i = 0; i < signals.size(); i++) {
+            series.put(signals.get(i), data.get(i));
+        }
+        TimeSeriesMulti correlated = TimeSeriesMulti.synchronizeSeries(series);
+        Point3DWithLabelDataset dataset = Point3DWithLabelDatasets.build(correlated.getValues().get(signals.get(0)), 
+                                                correlated.getValues().get(signals.get(1)), 
+                                                correlated.getValues().get(signals.get(2)), 
+                                                Collections.nCopies(correlated.getValues().get(signals.get(0)).size(), "label"));
+        BufferedImage image = new BufferedImage(800, 600, BufferedImage.TYPE_3BYTE_BGR);
+        Bubble2DGraphRenderer renderer = new Bubble2DGraphRenderer(800, 600);
+        renderer.draw(image.createGraphics(), dataset);
+        ImageIO.write(image, "png", new File(filename));
+    }
+}
---
+++ graphene-rrdtool/src/test/java/org/epics/graphene/rrdtool/RrdToolBubbleGraphMainSample.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright (C) 2012 University of Michigan
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.graphene.rrdtool;
+
+/**
+ *
+ * @author carcassi
+ */
+public class RrdToolBubbleGraphMainSample {
+    public static void main(String[] args) throws Exception {
+        RrdToolBubbleGraphMain.main(new String[] {"/media/sf_Shared/aru41_setpoint_5602.rrd:Temp1:AVERAGE",
+            "/media/sf_Shared/aru41_setpoint_5602.rrd:Temp2:AVERAGE",
+            "/media/sf_Shared/aru41_setpoint_5602.rrd:Temp3:AVERAGE",
+            "/media/sf_Shared/dc2-5-17_cpu_system_54823.rrd:cpu_system:AVERAGE",
+            "-s", "20121201000000", "-e", "20121215000000", "-o", "test"});
+    }
+}
