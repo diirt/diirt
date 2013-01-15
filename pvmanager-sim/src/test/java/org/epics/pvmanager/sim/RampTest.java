b08e7b82eb81a0d0dd54957471713e743d1670ab
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-01-15 14:31:49 -0500

    sim: ramp handles correctly descending ramps.

--- pvmanager-sim/src/test/java/org/epics/pvmanager/sim/RampTest.java
+++ pvmanager-sim/src/test/java/org/epics/pvmanager/sim/RampTest.java
@@ -17,7 +17,7 @@ import static org.hamcrest.CoreMatchers.*;
 public class RampTest {
 
     @Test
-    public void values() {
+    public void values1() {
         // Creates the function
         Ramp ramp = new Ramp(-10.0, 10.0, 2.0, 1.0);
         VDouble value = ramp.nextValue();
@@ -57,5 +57,48 @@ public class RampTest {
         assertThat(value.getValue(), equalTo(10.0));
         value = ramp.nextValue();
         assertThat(value.getValue(), equalTo(-10.0));
+    }
+
+    @Test
+    public void values2() {
+        // Creates the function
+        Ramp ramp = new Ramp(-10.0, 10.0, -2.0, 1.0);
+        VDouble value = ramp.nextValue();
+
+        // Check limits
+        assertThat(value.getAlarmSeverity(), equalTo(AlarmSeverity.MAJOR));
+        assertThat(value.getAlarmName(), equalTo("HIHI"));
+        assertThat(value.getLowerCtrlLimit(), equalTo(-10.0));
+        assertThat(value.getLowerDisplayLimit(), equalTo(-10.0));
+        assertThat(value.getLowerAlarmLimit(), equalTo(-8.0));
+        assertThat(value.getLowerWarningLimit(), equalTo(-6.0));
+        assertThat(value.getUpperWarningLimit(), equalTo(6.0));
+        assertThat(value.getUpperAlarmLimit(), equalTo(8.0));
+        assertThat(value.getUpperDisplayLimit(), equalTo(10.0));
+        assertThat(value.getUpperCtrlLimit(), equalTo(10.0));
+        
+        assertThat(value.getValue(), equalTo(10.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(8.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(6.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(4.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(2.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(0.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(-2.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(-4.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(-6.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(-8.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(-10.0));
+        value = ramp.nextValue();
+        assertThat(value.getValue(), equalTo(10.0));
     }
 }
