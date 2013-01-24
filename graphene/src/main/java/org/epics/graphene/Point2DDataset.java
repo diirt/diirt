75a4c2b57be02fee8d529bb233fa6a6e9b5459ac
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-01-24 17:04:36 -0500

    datasets: javadoc update

--- graphene/src/main/java/org/epics/graphene/Point2DDataset.java
+++ graphene/src/main/java/org/epics/graphene/Point2DDataset.java
@@ -7,13 +7,31 @@ package org.epics.graphene;
 import org.epics.util.array.ListNumber;
 
 /**
+ * A dataset consisting on a set of 2D points.
+ * <p>
+ * It represents a list of ordered tuples of two values, and their statistical information.
+ * The order may not be meaningful, but can be used to identify the points.
  *
  * @author carcassi
  */
 public interface Point2DDataset {
     
+    /**
+     * The x values of the point.
+     * <p>
+     * If the dataset is empty, it returns an empty list.
+     * 
+     * @return the x values; never null
+     */
     public ListNumber getXValues();
     
+    /**
+     * The y values of the point.
+     * <p>
+     * If the dataset is empty, it returns an empty list.
+     * 
+     * @return the y values; never null
+     */
     public ListNumber getYValues();
     
     public double getXMinValue();
@@ -24,6 +42,13 @@ public interface Point2DDataset {
     
     public double getYMaxValue();
     
+    /**
+     * The number of points in the dataset.
+     * <p>
+     * This number matches the size of the list returned by {@link #getValues() }.
+     * 
+     * @return the number of values in this dataset
+     */
     public int getCount();
     
 }
