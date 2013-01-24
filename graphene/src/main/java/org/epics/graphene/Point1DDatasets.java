da9331d57112489721a664b62441284e18fe7222
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-01-24 17:55:38 -0500

    datasets: ListNumber wrapper to Point1DDataset.

---
+++ graphene/src/main/java/org/epics/graphene/Point1DDatasets.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.epics.graphene;
+
+import org.epics.util.array.ListNumber;
+
+/**
+ * Factory methods for wrapper datasets.
+ *
+ * @author carcassi
+ */
+public class Point1DDatasets {
+    
+    /**
+     * Wraps a {@link ListNumber} into a {@link Point1DDataset}.
+     * <p>
+     * It assumes the argument is either immutable or mutable but
+     * will not be changed in the future.
+     * 
+     * @param values the values for the dataset
+     * @return the dataset from the values; never null
+     */
+    public static Point1DDataset of(final ListNumber values) {
+        final Statistics statistics = StatisticsUtil.statisticsOf(values);
+        return new Point1DDataset() {
+
+            @Override
+            public ListNumber getValues() {
+                return values;
+            }
+
+            @Override
+            public Statistics getStatistics() {
+                return statistics;
+            }
+
+            @Override
+            public int getCount() {
+                return values.size();
+            }
+        };
+    }
+}
