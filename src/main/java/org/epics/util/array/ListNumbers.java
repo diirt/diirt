42eb8ec523206abacda183908b615479ef8f5990
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-02-19 15:23:25 -0500

    array: adding a sorted view for lists

---
+++ src/main/java/org/epics/util/array/ListNumbers.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C) 2012 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.util.array;
+
+/**
+ *
+ * @author carcassi
+ */
+public class ListNumbers {
+    public static SortedListView sortedView(ListNumber values) {
+        SortedListView view = new SortedListView(values);
+        SortedListView.quicksort(view);
+        return view;
+    }
+}
---
+++ src/test/java/org/epics/util/array/ListNumbersTest.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C) 2012 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.util.array;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author carcassi
+ */
+public class ListNumbersTest {
+    
+    @Test
+    public void sortedView1() {
+        ArrayDouble values = new ArrayDouble(5,3,1,4,2,0);
+        SortedListView sortedView = ListNumbers.sortedView(values);
+        assertThat(values, equalTo(new ArrayDouble(5,3,1,4,2,0)));
+        assertThat(sortedView, equalTo((ListNumber) new ArrayDouble(0,1,2,3,4,5)));
+        assertThat(sortedView.getIndexes(), equalTo((ListInt) new ArrayInt(5,2,4,1,3,0)));
+    }
+}
