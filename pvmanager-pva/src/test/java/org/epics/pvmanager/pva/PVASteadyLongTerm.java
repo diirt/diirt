1c52cc1c1396e501a646ceff81e74b326b2d2869
Matej Sekoranja <matej.sekoranja@cosylab.com>
2013-02-23 22:23:15 +0100

    pvmanager-pva missing license headers in test dir

--- pvmanager-pva/src/test/java/org/epics/pvmanager/pva/PVASteadyLongTerm.java
+++ pvmanager-pva/src/test/java/org/epics/pvmanager/pva/PVASteadyLongTerm.java
@@ -1,9 +1,8 @@
-package org.epics.pvmanager.pva;
 /**
  * Copyright (C) 2010-12 Brookhaven National Laboratory
  * All rights reserved. Use is subject to license terms.
  */
-
+package org.epics.pvmanager.pva;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import org.epics.pvmanager.PVReader;
