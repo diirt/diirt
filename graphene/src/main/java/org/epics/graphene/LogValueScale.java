f56a75b054bd674aab7762367f40be2b0cc565db
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-02-26 12:59:19 -0500

    graphene: utility method to create ref values given the range and resolution

--- graphene/src/main/java/org/epics/graphene/LogValueScale.java
+++ graphene/src/main/java/org/epics/graphene/LogValueScale.java
@@ -8,6 +8,8 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import static org.epics.graphene.ValueAxis.orderOfMagnitude;
+import org.epics.util.array.CircularBufferDouble;
+import org.epics.util.array.ListDouble;
 import org.epics.util.text.NumberFormats;
 
 /**
@@ -29,6 +31,31 @@ final class LogValueScale implements ValueScale {
     @Override
     public ValueAxis references(Range range, int minRefs, int maxRefs) {
         return null;
+    }
+    
+    static ListDouble generateReferenceValues(Range range, int subdivisionFactor) {
+        CircularBufferDouble values = new CircularBufferDouble(100000);
+        double minValue = range.getMinimum().doubleValue();
+        double maxValue = range.getMaximum().doubleValue();
+        int minExp = Math.getExponent(minValue);
+        int maxExp = Math.getExponent(maxValue);
+        
+        int currentExp = minExp;
+        while (currentExp <= maxExp) {
+            double currentOrder = Math.pow(10, currentExp);
+            if (currentOrder <= maxValue) {
+                values.addDouble(currentOrder);
+            }
+            for (int i = 0; i < subdivisionFactor; i++) {
+                double newValue = (currentOrder * 10 * i) / subdivisionFactor;
+                if ((newValue <= maxValue) && (newValue > currentOrder)) {
+                    values.addDouble(newValue);
+                }
+            }
+            currentExp++;
+        }
+        
+        return values;
     }
     
 }
--- graphene/src/test/java/org/epics/graphene/LogValueScaleTest.java
+++ graphene/src/test/java/org/epics/graphene/LogValueScaleTest.java
@@ -7,6 +7,9 @@ package org.epics.graphene;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
+import static org.epics.graphene.LinearValueScaleTest.assertAxisEquals;
+import org.epics.util.array.ArrayDouble;
+import org.epics.util.array.ListDouble;
 
 /**
  *
@@ -24,5 +27,42 @@ public class LogValueScaleTest {
     public void scaleValue2() {
         ValueScale logScale = ValueScales.logScale();
         assertThat(logScale.scaleValue(100, 10, 1000, -100, 100), equalTo(0.0));
+    }
+
+//    @Test
+//    public void references1() {
+//        ValueScale logScale = ValueScales.logScale();
+//        ValueAxis axis = logScale.references(RangeUtil.range(1.0, 1000.0), 2, 4);
+//        assertAxisEquals(1.0, 1000.0, new double[]{1.0, 10.0, 100.0, 1000.0}, new String[]{"1", "10", "100", "1000"}, axis);
+//    }
+    
+    @Test
+    public void generateReferenceValues1() {
+        assertThat(LogValueScale.generateReferenceValues(RangeUtil.range(1, 10), 1), equalTo((ListDouble) new ArrayDouble(1, 10)));
+    }
+    
+    @Test
+    public void generateReferenceValues2() {
+        assertThat(LogValueScale.generateReferenceValues(RangeUtil.range(1, 10), 2), equalTo((ListDouble) new ArrayDouble(1, 5, 10)));
+    }
+    
+    @Test
+    public void generateReferenceValues3() {
+        assertThat(LogValueScale.generateReferenceValues(RangeUtil.range(1, 10), 5), equalTo((ListDouble) new ArrayDouble(1, 2, 4, 6, 8, 10)));
+    }
+    
+    @Test
+    public void generateReferenceValues4() {
+        assertThat(LogValueScale.generateReferenceValues(RangeUtil.range(1, 10), 10), equalTo((ListDouble) new ArrayDouble(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)));
+    }
+    
+    @Test
+    public void generateReferenceValues5() {
+        assertThat(LogValueScale.generateReferenceValues(RangeUtil.range(1, 3), 10), equalTo((ListDouble) new ArrayDouble(1, 2, 3)));
+    }
+    
+    @Test
+    public void generateReferenceValues6() {
+        assertThat(LogValueScale.generateReferenceValues(RangeUtil.range(1, 1.5), 100), equalTo((ListDouble) new ArrayDouble(1, 1.1, 1.2, 1.3, 1.4, 1.5)));
     }
 }
