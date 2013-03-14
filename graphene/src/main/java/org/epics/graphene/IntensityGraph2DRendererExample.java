8f1fa70d61224d6e93d86e24276b50e60d8f7739
Nitish Paradkar <nitishp@umich.edu>
2013-03-14 14:33:54 -0400

    [no commit message]

---
+++ graphene/src/main/java/org/epics/graphene/IntensityGraph2DRendererExample.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.epics.graphene;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author nitishp
+ */
+public class IntensityGraph2DRendererExample {
+    public static void main(String[] args) throws IOException
+    {
+    Cell2DDataset data = Cell2DDatasets.linearRange(new Cell2DDatasets.Function2D() {
+
+        @Override
+        public double getValue(double x, double y) {
+            return x+y;
+        }
+    }, RangeUtil.range(-10, 10), 10, RangeUtil.range(-10, 10), 10);
+    BufferedImage image = new BufferedImage(640, 480, BufferedImage.TYPE_3BYTE_BGR);
+    Graphics2D g = (Graphics2D) image.getGraphics();
+    IntensityGraph2DRenderer renderer = new IntensityGraph2DRenderer(640,480);
+    renderer.draw(g, data);
+    ImageIO.write(image, "png", new File("IntensityGraph.png"));
+}
+}
