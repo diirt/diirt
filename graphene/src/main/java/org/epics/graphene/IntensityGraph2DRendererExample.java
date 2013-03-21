d0c7d15d46491eba3e9946e8b0e6d08e99fe71d3
Nitish Paradkar <nitishp@umich.edu>
2013-03-21 12:18:08 -0400

    Got the pixels on the Intensity Graph to line up correctly No part of graph is cut off now

--- graphene/src/main/java/org/epics/graphene/IntensityGraph2DRendererExample.java
+++ graphene/src/main/java/org/epics/graphene/IntensityGraph2DRendererExample.java
@@ -16,18 +16,18 @@ import javax.imageio.ImageIO;
  */
 public class IntensityGraph2DRendererExample {
     public static void main(String[] args) throws IOException
-    {
-    Cell2DDataset data = Cell2DDatasets.linearRange(new Cell2DDatasets.Function2D() {
-
-        @Override
-        public double getValue(double x, double y) {
-            return x+y;
+        {
+            Cell2DDataset data = Cell2DDatasets.linearRange(new Cell2DDatasets.Function2D() {
+                
+                @Override
+                public double getValue(double x, double y) {
+                    return x+y;
+                }
+            }, RangeUtil.range(0, 10), 10, RangeUtil.range(0, 10), 10);
+            BufferedImage image = new BufferedImage(640, 480, BufferedImage.TYPE_3BYTE_BGR);
+            Graphics2D g = (Graphics2D) image.getGraphics();
+            IntensityGraph2DRenderer renderer = new IntensityGraph2DRenderer(640,480);
+            renderer.draw(g, data);
+            ImageIO.write(image, "png", new File("IntensityGraph.png"));
         }
-    }, RangeUtil.range(-10, 10), 10, RangeUtil.range(-10, 10), 10);
-    BufferedImage image = new BufferedImage(640, 480, BufferedImage.TYPE_3BYTE_BGR);
-    Graphics2D g = (Graphics2D) image.getGraphics();
-    IntensityGraph2DRenderer renderer = new IntensityGraph2DRenderer(640,480);
-    renderer.draw(g, data);
-    ImageIO.write(image, "png", new File("IntensityGraph.png"));
-}
-}
+    }
