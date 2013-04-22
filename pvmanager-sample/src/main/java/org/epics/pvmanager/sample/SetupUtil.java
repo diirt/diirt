7f45d98713ccc2101fb4fb179a87696d3f2145d3
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-04-22 13:09:20 -0400

    sample: use all math operators

--- pvmanager-sample/src/main/java/org/epics/pvmanager/sample/SetupUtil.java
+++ pvmanager-sample/src/main/java/org/epics/pvmanager/sample/SetupUtil.java
@@ -6,8 +6,10 @@ package org.epics.pvmanager.sample;
 
 import org.epics.pvmanager.CompositeDataSource;
 import org.epics.pvmanager.PVManager;
+import org.epics.pvmanager.formula.ArrayFunctionSet;
 import org.epics.pvmanager.formula.FormulaRegistry;
 import org.epics.pvmanager.formula.MathFunctionSet;
+import org.epics.pvmanager.formula.NumberOperatorFunctionSet;
 import org.epics.pvmanager.jca.JCADataSource;
 import org.epics.pvmanager.jca.JCADataSourceBuilder;
 import org.epics.pvmanager.loc.LocalDataSource;
@@ -28,7 +30,9 @@ public class SetupUtil {
         dataSource.putDataSource("sys", new SystemDataSource());
         dataSource.setDefaultDataSource("ca");
         PVManager.setDefaultDataSource(dataSource);
+        FormulaRegistry.getDefault().registerFormulaFunctionSet(new ArrayFunctionSet());
         FormulaRegistry.getDefault().registerFormulaFunctionSet(new MathFunctionSet());
+        FormulaRegistry.getDefault().registerFormulaFunctionSet(new NumberOperatorFunctionSet());
     }
     public static void defaultCASetupForSwing() {
         PVManager.setDefaultNotificationExecutor(Executors.swingEDT());
