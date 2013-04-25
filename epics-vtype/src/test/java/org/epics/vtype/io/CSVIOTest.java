3b07757478b6ad121b617b2d57d484fc82d2bf26
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-04-25 10:42:22 -0400

    vtype: refactor io tests

--- epics-vtype/src/test/java/org/epics/vtype/io/CSVIOTest.java
+++ epics-vtype/src/test/java/org/epics/vtype/io/CSVIOTest.java
@@ -24,14 +24,24 @@ public class CSVIOTest {
     }
 
     @Test
-    public void exportVNumber() {
+    public void exportVNumber1() {
         VNumber value = ValueFactory.newVDouble(1.0, newTime(Timestamp.of(0, 0)));
-        
         CSVIO io = new CSVIO();
+        exportTest(io, value, "\"1969/12/31 19:00:00.0 -0500\" NONE NONE 1.0");
+    }
+
+    @Test
+    public void exportVNumber2() {
+        VNumber value = ValueFactory.newVInt(10, alarmNone(), newTime(Timestamp.of(90, 0)), displayNone());
+        CSVIO io = new CSVIO();
+        exportTest(io, value, "\"1969/12/31 19:01:30.0 -0500\" NONE NONE 10.0");
+    }
+    
+    public static void exportTest(CSVIO io, Object value, String csv) {
         assertThat(io.canExport(value), equalTo(true));
         StringWriter writer = new StringWriter();
         io.export(value, writer);
         System.out.println(writer.toString());
-        assertThat(writer.toString(), equalTo("\"1969/12/31 19:00:00.0 -0500\" NONE NONE 1.0"));
+        assertThat(writer.toString(), equalTo(csv));
     }
 }
