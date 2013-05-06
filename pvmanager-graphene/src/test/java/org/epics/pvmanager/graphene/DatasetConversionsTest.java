53df0e7122f5854013d7faec3df151e1f39ca852
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-05-06 09:53:56 -0400

    graphene: adding utility class to convert vTables into point2DDatasets.

---
+++ pvmanager-graphene/src/test/java/org/epics/pvmanager/graphene/DatasetConversionsTest.java
@@ -0,0 +1,106 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.graphene;
+
+import java.util.Arrays;
+import org.epics.graphene.Point2DDataset;
+import org.epics.util.array.ArrayDouble;
+import org.epics.util.array.ListNumber;
+import org.epics.vtype.VTable;
+import org.epics.vtype.ValueFactory;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author carcassi
+ */
+public class DatasetConversionsTest {
+    
+    public DatasetConversionsTest() {
+    }
+
+    @Test
+    public void point2DDatsetFromVTable1() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class, double.class),
+                Arrays.asList("x", "y"), Arrays.<Object>asList(new ArrayDouble(1,2,3), new ArrayDouble(5,4,6)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, "x", "y");
+        assertThat(dataset.getCount(), equalTo(3));
+        assertThat(dataset.getXValues(), equalTo((ListNumber) new ArrayDouble(1,2,3)));
+        assertThat(dataset.getYValues(), equalTo((ListNumber) new ArrayDouble(5,4,6)));
+    }
+
+    @Test
+    public void point2DDatsetFromVTable2() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class, double.class),
+                Arrays.asList("x", "y"), Arrays.<Object>asList(new ArrayDouble(1,2,3), new ArrayDouble(5,4,6)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, "x", null);
+        assertThat(dataset.getCount(), equalTo(3));
+        assertThat(dataset.getXValues(), equalTo((ListNumber) new ArrayDouble(1,2,3)));
+        assertThat(dataset.getYValues(), equalTo((ListNumber) new ArrayDouble(0,1,2)));
+    }
+
+    @Test
+    public void point2DDatsetFromVTable3() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class, double.class),
+                Arrays.asList("x", "y"), Arrays.<Object>asList(new ArrayDouble(1,2,3), new ArrayDouble(5,4,6)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, null, "y");
+        assertThat(dataset.getCount(), equalTo(3));
+        assertThat(dataset.getXValues(), equalTo((ListNumber) new ArrayDouble(0,1,2)));
+        assertThat(dataset.getYValues(), equalTo((ListNumber) new ArrayDouble(5,4,6)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void point2DDatsetFromVTable4() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class, double.class),
+                Arrays.asList("x", "y"), Arrays.<Object>asList(new ArrayDouble(1,2,3), new ArrayDouble(5,4,6)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, null, "z");
+    }
+
+    @Test
+    public void point2DDatsetFromVTable5() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class, double.class),
+                Arrays.asList("x", "y"), Arrays.<Object>asList(new ArrayDouble(1,2,3), new ArrayDouble(5,4,6)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, null, null);
+        assertThat(dataset.getCount(), equalTo(3));
+        assertThat(dataset.getXValues(), equalTo((ListNumber) new ArrayDouble(1,2,3)));
+        assertThat(dataset.getYValues(), equalTo((ListNumber) new ArrayDouble(5,4,6)));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void point2DDatsetFromVTable6() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class, String.class),
+                Arrays.asList("x", "y"), Arrays.<Object>asList(new ArrayDouble(1,2,3), Arrays.asList("a", "b", "c")));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, null, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void point2DDatsetFromVTable7() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class),
+                Arrays.asList("x"), Arrays.<Object>asList(new ArrayDouble(1,2,3)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, null, null);
+    }
+
+    @Test
+    public void point2DDatsetFromVTable8() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(double.class, String.class, double.class),
+                Arrays.asList("x", "y", "z"), Arrays.<Object>asList(new ArrayDouble(1,2,3), Arrays.asList("a", "b", "c"), new ArrayDouble(5,4,6)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, null, null);
+        assertThat(dataset.getCount(), equalTo(3));
+        assertThat(dataset.getXValues(), equalTo((ListNumber) new ArrayDouble(1,2,3)));
+        assertThat(dataset.getYValues(), equalTo((ListNumber) new ArrayDouble(5,4,6)));
+    }
+
+    @Test
+    public void point2DDatsetFromVTable9() {
+        VTable data = ValueFactory.newVTable(Arrays.<Class<?>>asList(String.class, double.class, double.class),
+                Arrays.asList("x", "y", "z"), Arrays.<Object>asList(Arrays.asList("a", "b", "c"), new ArrayDouble(1,2,3), new ArrayDouble(5,4,6)));
+        Point2DDataset dataset = DatasetConversions.point2DDatasetFromVTable(data, null, null);
+        assertThat(dataset.getCount(), equalTo(3));
+        assertThat(dataset.getXValues(), equalTo((ListNumber) new ArrayDouble(1,2,3)));
+        assertThat(dataset.getYValues(), equalTo((ListNumber) new ArrayDouble(5,4,6)));
+    }
+}
