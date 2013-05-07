c5c4300d51f3f55d42d377bff0665d204d4ce091
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-05-07 11:45:38 -0400

    sample: add file datasource in sample configuration

--- pvmanager-sample/src/main/java/org/epics/pvmanager/sample/SetupUtil.java
+++ pvmanager-sample/src/main/java/org/epics/pvmanager/sample/SetupUtil.java
@@ -6,11 +6,11 @@ package org.epics.pvmanager.sample;
 
 import org.epics.pvmanager.CompositeDataSource;
 import org.epics.pvmanager.PVManager;
+import org.epics.pvmanager.file.FileDataSource;
 import org.epics.pvmanager.formula.ArrayFunctionSet;
 import org.epics.pvmanager.formula.FormulaRegistry;
 import org.epics.pvmanager.formula.MathFunctionSet;
 import org.epics.pvmanager.formula.NumberOperatorFunctionSet;
-import org.epics.pvmanager.jca.JCADataSource;
 import org.epics.pvmanager.jca.JCADataSourceBuilder;
 import org.epics.pvmanager.loc.LocalDataSource;
 import org.epics.pvmanager.sim.SimulationDataSource;
@@ -28,6 +28,7 @@ public class SetupUtil {
         dataSource.putDataSource("ca", new JCADataSourceBuilder().build());
         dataSource.putDataSource("loc", new LocalDataSource());
         dataSource.putDataSource("sys", new SystemDataSource());
+        dataSource.putDataSource("file", new FileDataSource());
         dataSource.setDefaultDataSource("ca");
         PVManager.setDefaultDataSource(dataSource);
         FormulaRegistry.getDefault().registerFormulaFunctionSet(new ArrayFunctionSet());
