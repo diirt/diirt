d6966e421f8337e9d2ef8cd4c2ed835d89c726d7
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-05-14 09:59:59 -0400

    formula: adding columnOf as a FormulaFunction.

---
+++ pvmanager-extra/src/main/java/org/epics/pvmanager/formula/TableFunctionSet.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
+package org.epics.pvmanager.formula;
+
+/**
+ * A function set for table operations.
+ *
+ * @author carcassi
+ */
+public class TableFunctionSet extends FormulaFunctionSet {
+
+    /**
+     * Creates a new set.
+     */
+    public TableFunctionSet() {
+        super(new FormulaFunctionSetDescription("table", "Function to aggregate and manipulate tables")
+                .addFormulaFunction(new ColumnOfVTableFunction())
+                );
+    }
+
+
+}
