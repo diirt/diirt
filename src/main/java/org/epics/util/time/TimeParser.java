493b8eade48aa652c20362cd474095f1ffd74f78
shroffk <devnull@localhost>
2013-05-16 08:37:11 -0400

    Adding functionality to define a range specifying both start and end in last/ago terms

--- src/main/java/org/epics/util/time/TimeParser.java
+++ src/main/java/org/epics/util/time/TimeParser.java
@@ -4,6 +4,8 @@
  */
 package org.epics.util.time;
 
+import java.text.ParseException;
+import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -42,7 +44,7 @@ public class TimeParser {
 
 	Pattern lastNUnitsPattern = Pattern
 		.compile(
-			"last\\s+(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks).*",
+			"last\\s*(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks).*",
 			Pattern.CASE_INSENSITIVE);
 	Matcher lastNUnitsMatcher = lastNUnitsPattern.matcher(time);
 	while (lastNUnitsMatcher.find()) {
@@ -52,7 +54,7 @@ public class TimeParser {
 	}
 
 	Pattern nUnitsAgoPattern = Pattern.compile(
-		"(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks)\\s+ago",
+		"(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks)\\s*ago",
 		Pattern.CASE_INSENSITIVE);
 	Matcher nUnitsAgoMatcher = nUnitsAgoPattern.matcher(time);
 	while (nUnitsAgoMatcher.find()) {
@@ -81,63 +83,97 @@ public class TimeParser {
     }
 
     public static TimeInterval getTimeInterval(String time) {
-	int quantity = 0;
-	String unit = "";
-
-	Pattern lastNUnitsPattern = Pattern
-		.compile(
-			"last\\s+(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks).*",
-			Pattern.CASE_INSENSITIVE);
-	Matcher lastNUnitsMatcher = lastNUnitsPattern.matcher(time);
-	while (lastNUnitsMatcher.find()) {
-	    quantity = "".equals(lastNUnitsMatcher.group(1)) ? 1 : Integer
-		    .valueOf(lastNUnitsMatcher.group(1));
-	    unit = lastNUnitsMatcher.group(2);
-	}
-
-	Pattern nUnitsAgoPattern = Pattern.compile(
-		"(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks)\\s+ago",
-		Pattern.CASE_INSENSITIVE);
-	Matcher nUnitsAgoMatcher = nUnitsAgoPattern.matcher(time);
-	while (nUnitsAgoMatcher.find()) {
-	    quantity = "".equals(nUnitsAgoMatcher.group(1)) ? 1 : Integer
-		    .valueOf(nUnitsAgoMatcher.group(1));
-	    unit = nUnitsAgoMatcher.group(2);
-	}
-	unit = unit.toLowerCase();
-	switch (unit) {
-	case "min":
-	case "mins":
-	    return TimeInterval.between(
-		    Timestamp.now().minus(TimeDuration.ofMinutes(quantity)),
-		    Timestamp.now());
-	case "hour":
-	case "hours":
-	    return TimeInterval.between(
-		    Timestamp.now().minus(TimeDuration.ofHours(quantity)),
-		    Timestamp.now());
-	case "day":
-	case "days":
-	    return TimeInterval.between(
-		    Timestamp.now().minus(TimeDuration.ofHours(quantity * 24)),
-		    Timestamp.now());
-	case "week":
-	case "weeks":
-	    return TimeInterval.between(
-		    Timestamp.now().minus(
-			    TimeDuration.ofHours(quantity * 24 * 7)),
-		    Timestamp.now());
-	default:
-	    break;
-	}
-	return null;
+	return getTimeInterval(time, "now");
     }
 
+    public static TimeInterval getTimeInterval(String start, String end) {
+	return TimeInterval.between(getTimeStamp(start), getTimeStamp(end));
+    }
+
+    /**
+     * A Helper function to help you convert various string represented time
+     * definition to an absolute Timestamp.
+     * 
+     * i.e.
+     * 
+     * @param time
+     * @return
+     */
     public static Timestamp getTimeStamp(String time) {
 	if (time.equalsIgnoreCase("now")) {
 	    return Timestamp.now();
 	} else {
-	    return null;
+	    int quantity = 0;
+	    String unit = "";
+	    Pattern lastNUnitsPattern = Pattern
+		    .compile(
+			    "last\\s*(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks).*",
+			    Pattern.CASE_INSENSITIVE);
+	    Matcher lastNUnitsMatcher = lastNUnitsPattern.matcher(time);
+	    while (lastNUnitsMatcher.find()) {
+		quantity = "".equals(lastNUnitsMatcher.group(1)) ? 1 : Integer
+			.valueOf(lastNUnitsMatcher.group(1));
+		unit = lastNUnitsMatcher.group(2).toLowerCase();
+		switch (unit) {
+		case "min":
+		case "mins":
+		    return Timestamp.now().minus(
+			    TimeDuration.ofMinutes(quantity));
+		case "hour":
+		case "hours":
+		    return Timestamp.now()
+			    .minus(TimeDuration.ofHours(quantity));
+		case "day":
+		case "days":
+		    return Timestamp.now().minus(
+			    TimeDuration.ofHours(quantity * 24));
+		case "week":
+		case "weeks":
+		    return Timestamp.now().minus(
+			    TimeDuration.ofHours(quantity * 24 * 7));
+		default:
+		    break;
+		}
+	    }
+
+	    Pattern nUnitsAgoPattern = Pattern
+		    .compile(
+			    "(\\d*)\\s*(min|mins|hour|hours|day|days|week|weeks)\\s*ago",
+			    Pattern.CASE_INSENSITIVE);
+	    Matcher nUnitsAgoMatcher = nUnitsAgoPattern.matcher(time);
+	    while (nUnitsAgoMatcher.find()) {
+		quantity = "".equals(nUnitsAgoMatcher.group(1)) ? 1 : Integer
+			.valueOf(nUnitsAgoMatcher.group(1));
+		unit = nUnitsAgoMatcher.group(2).toLowerCase();
+		switch (unit) {
+		case "min":
+		case "mins":
+		    return Timestamp.now().minus(
+			    TimeDuration.ofMinutes(quantity));
+
+		case "hour":
+		case "hours":
+		    return Timestamp.now()
+			    .minus(TimeDuration.ofHours(quantity));
+		case "day":
+		case "days":
+		    return Timestamp.now().minus(
+			    TimeDuration.ofHours(quantity * 24));
+		case "week":
+		case "weeks":
+		    return Timestamp.now().minus(
+			    TimeDuration.ofHours(quantity * 24 * 7));
+		default:
+		    break;
+		}
+	    }
+	    TimestampFormat format = new TimestampFormat(
+		    "yyyy-MM-dd'T'HH:mm:ss");
+	    try {
+		return format.parse(time);
+	    } catch (ParseException e) {
+		return null;
+	    }
 	}
     }
 }
--- src/test/java/org/epics/util/time/TimeParserTest.java
+++ src/test/java/org/epics/util/time/TimeParserTest.java
@@ -4,6 +4,9 @@
  */
 package org.epics.util.time;
 
+import java.text.ParseException;
+import java.util.TimeZone;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -152,127 +155,131 @@ public class TimeParserTest {
 	Assert.assertEquals("Failed to get TimeInterval for last min", 60,
 		lastMin.getStart().durationBetween(lastMin.getEnd()).getSec());
 	TimeInterval lastHour = TimeParser.getTimeInterval("last hour");
-	Assert.assertEquals(
-		"Failed to get TimeInterval for last hour",
+	Assert.assertEquals("Failed to get TimeInterval for last hour",
 		(60 * 60),
-		(lastHour.getStart().durationBetween(lastMin.getEnd()).getSec()),
-		1);
+		(lastHour.getStart().durationBetween(lastHour.getEnd())
+			.getSec()), 0);
 	TimeInterval lastDay = TimeParser.getTimeInterval("last day");
-	Assert.assertEquals("Failed to get TimeInterval for last day",
-		(60 * 60 * 24),
-		(lastDay.getStart().durationBetween(lastMin.getEnd()).getSec()),1);
-	TimeInterval lastWeek = TimeParser.getTimeInterval("last week");
 	Assert.assertEquals(
-		"Failed to get TimeInterval for last week",
+		"Failed to get TimeInterval for last day",
+		(60 * 60 * 24),
+		(lastDay.getStart().durationBetween(lastDay.getEnd()).getSec()),
+		0);
+	TimeInterval lastWeek = TimeParser.getTimeInterval("last week");
+	Assert.assertEquals("Failed to get TimeInterval for last week",
 		(60 * 60 * 24 * 7),
-		(lastWeek.getStart().durationBetween(lastMin.getEnd()).getSec()),1);
+		(lastWeek.getStart().durationBetween(lastWeek.getEnd())
+			.getSec()), 0);
 
 	// "last 5 mins", "last 5 hours", "last 5 days", "last 5 weeks"
 	TimeInterval last5Min = TimeParser.getTimeInterval("last 5 mins");
 	Assert.assertEquals("Failed to get TimeInterval for last 5 mins",
-		(60 * 5), (last5Min.getStart()
-			.durationBetween(lastMin.getEnd()).getSec()),1);
+		(60 * 5),
+		(last5Min.getStart().durationBetween(last5Min.getEnd())
+			.getSec()), 0);
 	TimeInterval last5Hour = TimeParser.getTimeInterval("last 5 hours");
 	Assert.assertEquals("Failed to get TimeInterval for last 5 hours",
 		(60 * 60 * 5),
-		(last5Hour.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last5Hour.getStart().durationBetween(last5Hour.getEnd())
+			.getSec()), 0);
 	TimeInterval last5Day = TimeParser.getTimeInterval("last 5 days");
-	Assert.assertEquals(
-		"Failed to get TimeInterval for last 5 days",
+	Assert.assertEquals("Failed to get TimeInterval for last 5 days",
 		(60 * 60 * 24 * 5),
-		(last5Day.getStart().durationBetween(lastMin.getEnd()).getSec()),1);
+		(last5Day.getStart().durationBetween(last5Day.getEnd())
+			.getSec()), 0);
 	TimeInterval last5Week = TimeParser.getTimeInterval("last 5 weeks");
 	Assert.assertEquals("Failed to get TimeInterval for last 5 weeks",
 		(60 * 60 * 24 * 7 * 5),
-		(last5Week.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last5Week.getStart().durationBetween(last5Week.getEnd())
+			.getSec()), 0);
 
 	// "1 min ago", "1 hours ago", "1 days ago", "1 weeks ago"
 	TimeInterval oneMinAgo = TimeParser.getTimeInterval("1 min ago");
 	Assert.assertEquals("Failed to get TimeInterval for 1 min ago", (60),
-		(oneMinAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(oneMinAgo.getStart().durationBetween(oneMinAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval oneHourAgo = TimeParser.getTimeInterval("1 hour ago");
 	Assert.assertEquals("Failed to get TimeInterval for 1 hour ago",
 		(60 * 60),
-		(oneHourAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(oneHourAgo.getStart().durationBetween(oneHourAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval oneDayAgo = TimeParser.getTimeInterval("1 day ago");
 	Assert.assertEquals("Failed to get TimeInterval for 1 days ago",
 		(60 * 60 * 24),
-		(oneDayAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(oneDayAgo.getStart().durationBetween(oneDayAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval oneWeekAgo = TimeParser.getTimeInterval("1 week ago");
 	Assert.assertEquals("Failed to get TimeInterval for 1 week ago",
 		(60 * 60 * 24 * 7),
-		(oneWeekAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(oneWeekAgo.getStart().durationBetween(oneWeekAgo.getEnd())
+			.getSec()), 0);
 
 	// "5 mins ago", "5 hours ago", "5 days ago", "5 weeks ago"
 	TimeInterval fiveMinsAgo = TimeParser.getTimeInterval("5 mins ago");
 	Assert.assertEquals("Failed to get TimeInterval for 5 mins ago",
 		(60 * 5),
-		(fiveMinsAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(fiveMinsAgo.getStart().durationBetween(fiveMinsAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval fiveHoursAgo = TimeParser.getTimeInterval("5 hours ago");
 	Assert.assertEquals("Failed to get TimeInterval for 5 hours ago",
 		(60 * 60 * 5),
-		(fiveHoursAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(fiveHoursAgo.getStart().durationBetween(fiveHoursAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval fiveDaysAgo = TimeParser.getTimeInterval("5 days ago");
 	Assert.assertEquals("Failed to get TimeInterval for 5 days ago",
 		(60 * 60 * 24 * 5),
-		(fiveDaysAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(fiveDaysAgo.getStart().durationBetween(fiveDaysAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval fiveWeeksAgo = TimeParser.getTimeInterval("5 weeks ago");
 	Assert.assertEquals("Failed to get TimeInterval for 5 week ago",
 		(60 * 60 * 24 * 7 * 5), (fiveWeeksAgo.getStart()
-			.durationBetween(lastMin.getEnd()).getSec()),1);
+			.durationBetween(fiveWeeksAgo.getEnd()).getSec()), 0);
 
 	// Check case insensitivity Last 4 Mins, Last 4 Hours, Last 4 Days, Last
 	// 4 WEEKS
 	TimeInterval last4Min = TimeParser.getTimeInterval("Last 4 Mins");
 	Assert.assertEquals("Failed to get TimeInterval for Last 4 Mins",
-		(60 * 4), (last4Min.getStart()
-			.durationBetween(lastMin.getEnd()).getSec()),1);
+		(60 * 4),
+		(last4Min.getStart().durationBetween(last4Min.getEnd())
+			.getSec()), 0);
 	TimeInterval last4Hour = TimeParser.getTimeInterval("Last 4 Hours");
 	Assert.assertEquals("Failed to get TimeInterval for Last 4 Hours",
 		(60 * 60 * 4),
-		(last4Hour.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last4Hour.getStart().durationBetween(last4Hour.getEnd())
+			.getSec()), 0);
 	TimeInterval last4Day = TimeParser.getTimeInterval("Last 4 Day");
-	Assert.assertEquals(
-		"Failed to get TimeInterval for Last 4 Day",
+	Assert.assertEquals("Failed to get TimeInterval for Last 4 Day",
 		(60 * 60 * 24 * 4),
-		(last4Day.getStart().durationBetween(lastMin.getEnd()).getSec()),1);
+		(last4Day.getStart().durationBetween(last4Day.getEnd())
+			.getSec()), 0);
 	TimeInterval last4Week = TimeParser.getTimeInterval("Last 4 WEEKS");
 	Assert.assertEquals("Failed to get TimeInterval for Last 4 WEEKS",
 		(60 * 60 * 24 * 7 * 4),
-		(last4Week.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last4Week.getStart().durationBetween(last4Week.getEnd())
+			.getSec()), 0);
 
 	// Check incorrect units in terms of plurality last 3 min, last 3 hour,
 	// last 3 day, last 3 week
 	TimeInterval last3Min = TimeParser.getTimeInterval("last 3 min");
 	Assert.assertEquals("Failed to get TimeInterval for last 3 min",
-		(60 * 3), (last3Min.getStart()
-			.durationBetween(lastMin.getEnd()).getSec()),1);
+		(60 * 3),
+		(last3Min.getStart().durationBetween(last3Min.getEnd())
+			.getSec()), 0);
 	TimeInterval last3Hour = TimeParser.getTimeInterval("last 3 hour");
 	Assert.assertEquals("Failed to get TimeInterval for last 3 hour",
 		(60 * 60 * 3),
-		(last3Hour.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last3Hour.getStart().durationBetween(last3Hour.getEnd())
+			.getSec()), 0);
 	TimeInterval last3Day = TimeParser.getTimeInterval("last 3 day");
-	Assert.assertEquals(
-		"Failed to get TimeInterval for last 3 day",
+	Assert.assertEquals("Failed to get TimeInterval for last 3 day",
 		(60 * 60 * 24 * 3),
-		(last3Day.getStart().durationBetween(lastMin.getEnd()).getSec()),1);
+		(last3Day.getStart().durationBetween(last3Day.getEnd())
+			.getSec()), 0);
 	TimeInterval last3Week = TimeParser.getTimeInterval("last 3 week");
 	Assert.assertEquals("Failed to get TimeInterval for last 3 week",
 		(60 * 60 * 24 * 7 * 3),
-		(last3Week.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last3Week.getStart().durationBetween(last3Week.getEnd())
+			.getSec()), 0);
 
 	// Check missing space between time quantity and unit last 2mins, last
 	// 2hours, last 2days, last 2weeks, 2mins ago, 2hours ago, 2days ago,
@@ -280,41 +287,62 @@ public class TimeParserTest {
 	TimeInterval last2Mins = TimeParser.getTimeInterval("last 2mins");
 	Assert.assertEquals("Failed to get TimeInterval for last 2mins",
 		(60 * 2),
-		(last2Mins.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last2Mins.getStart().durationBetween(last2Mins.getEnd())
+			.getSec()), 0);
 	TimeInterval last2Hours = TimeParser.getTimeInterval("last 2hours");
 	Assert.assertEquals("Failed to get TimeInterval for last 2hours",
 		(60 * 60 * 2),
-		(last2Hours.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last2Hours.getStart().durationBetween(last2Hours.getEnd())
+			.getSec()), 0);
 	TimeInterval last2Days = TimeParser.getTimeInterval("last 2days");
 	Assert.assertEquals("Failed to get TimeInterval for last 2days",
 		(60 * 60 * 24 * 2),
-		(last2Days.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last2Days.getStart().durationBetween(last2Days.getEnd())
+			.getSec()), 0);
 	TimeInterval last2Weeks = TimeParser.getTimeInterval("last 2weeks");
 	Assert.assertEquals("Failed to get TimeInterval for last 2weeks",
 		(60 * 60 * 24 * 7 * 2),
-		(last2Weeks.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(last2Weeks.getStart().durationBetween(last2Weeks.getEnd())
+			.getSec()), 0);
 	TimeInterval twoMinsAgo = TimeParser.getTimeInterval("2mins ago");
 	Assert.assertEquals("Failed to get TimeInterval for 2mins ago",
 		(60 * 2),
-		(twoMinsAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(twoMinsAgo.getStart().durationBetween(twoMinsAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval twoHoursAgo = TimeParser.getTimeInterval("2hours ago");
 	Assert.assertEquals("Failed to get TimeInterval for 2hours ago",
 		(60 * 60 * 2),
-		(twoHoursAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(twoHoursAgo.getStart().durationBetween(twoHoursAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval twoDaysAgo = TimeParser.getTimeInterval("2days ago");
 	Assert.assertEquals("Failed to get TimeInterval for 2days ago",
 		(60 * 60 * 24 * 2),
-		(twoDaysAgo.getStart().durationBetween(lastMin.getEnd())
-			.getSec()),1);
+		(twoDaysAgo.getStart().durationBetween(twoDaysAgo.getEnd())
+			.getSec()), 0);
 	TimeInterval twoWeeksAgo = TimeParser.getTimeInterval("2weeks ago");
 	Assert.assertEquals("Failed to get TimeInterval for 2weeks ago",
 		(60 * 60 * 24 * 7 * 2), (twoWeeksAgo.getStart()
-			.durationBetween(lastMin.getEnd()).getSec()),1);
+			.durationBetween(twoWeeksAgo.getEnd()).getSec()), 0);
+    }
+
+    /**
+     * Test the creation of time intervals using from: and to:
+     */
+    @Test
+    public void getAbsoluteTimeInterval() {
+	TimeInterval oneMin = TimeParser.getTimeInterval("3 mins ago",
+		"2 mins ago");
+	Assert.assertEquals(
+		"Failed to get time Interval for String: from:3 mins ago to:2 mins ago",
+		60,
+		oneMin.getStart().durationBetween(oneMin.getEnd()).getSec(), 0);
+	// Explicitly define the time
+	String startTime = "1976-01-01T00:00:00";
+	String endTime = "1976-01-02T00:00:00";
+	TimeInterval oneDay = TimeParser.getTimeInterval(startTime, endTime);
+	Assert.assertEquals("Failed to get time Interval for String: from:"
+		+ startTime + " to:" + endTime, 60 * 60 * 24, oneDay.getStart()
+		.durationBetween(oneDay.getEnd()).getSec(), 0);
+
     }
 }
