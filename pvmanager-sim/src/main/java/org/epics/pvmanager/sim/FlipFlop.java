243ffe4962244780794299f056853efcf21713f3
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-07-10 12:56:13 -0400

    sim: renamed channel

--- pvmanager-sim/src/main/java/org/epics/pvmanager/sim/FlipFlop.java
+++ pvmanager-sim/src/main/java/org/epics/pvmanager/sim/FlipFlop.java
@@ -13,14 +13,14 @@ import static org.epics.vtype.ValueFactory.*;
  *
  * @author carcassi
  */
-public class FlipFlop extends SimFunction<VBoolean> {
+public class Flipflop extends SimFunction<VBoolean> {
 
     private boolean value = true;
 
     /**
      * Creates a flipflop that changes every 500 ms.
      */
-    public FlipFlop() {
+    public Flipflop() {
         this(0.5);
     }
 
@@ -29,7 +29,7 @@ public class FlipFlop extends SimFunction<VBoolean> {
      *
      * @param interval interval between samples in seconds
      */
-    public FlipFlop(Double interval) {
+    public Flipflop(Double interval) {
         super(interval, VBoolean.class);
         if (interval <= 0.0) {
             throw new IllegalArgumentException("Interval must be greater than zero (was " + interval + ")");
