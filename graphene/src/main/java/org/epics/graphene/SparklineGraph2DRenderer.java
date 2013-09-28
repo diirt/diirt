0126cd9eb06180a01503e0b4a7aead862579d8b3
Aaron Barber <asbarder@umich.edu>
2013-09-28 15:57:37 -0400

    Added SparklineGraph2DRenderer constructor, newUpdate method, and draw method. Added SparklineGraph2DRendererExample.java

--- graphene/src/main/java/org/epics/graphene/SparklineGraph2DRenderer.java
+++ graphene/src/main/java/org/epics/graphene/SparklineGraph2DRenderer.java
@@ -1,4 +1,7 @@
-
+/**
+ * Copyright (C) 2012 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
+ */
 package org.epics.graphene;
 
 import java.awt.*;
@@ -12,4 +15,28 @@ import org.epics.util.array.ListNumber;
  */
 public class SparklineGraph2DRenderer extends Graph2DRenderer<Graph2DRendererUpdate>{
     
+    /**
+     * Creates a new sparkline graph renderer.
+     * 
+     * @param imageWidth the graph width
+     * @param imageHeight the graph height
+     */    
+    public SparklineGraph2DRenderer(int imageWidth, int imageHeight){
+        super(imageWidth, imageHeight);
+    }
+    
+    @Override
+    public Graph2DRendererUpdate newUpdate() {
+        return new SparklineGraph2DRendererUpdate();
+    }
+
+    /**
+     * Draws the graph on the given graphics context.
+     * 
+     * @param g the graphics on which to display the data
+     * @param data the data to display
+     */
+    public void draw(Graphics2D g, Cell2DDataset data) {
+
+    }
 }
