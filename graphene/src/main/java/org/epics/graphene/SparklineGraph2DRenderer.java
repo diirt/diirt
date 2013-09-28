110d529bc850c0202be8e283d3aef24fa9bf3a70
Samuel Dallstream <sjdallst@gmail.com>
2013-09-28 15:42:09 -0400

    Added SparklineGraph2DRenderer

---
+++ graphene/src/main/java/org/epics/graphene/SparklineGraph2DRenderer.java
@@ -0,0 +1,15 @@
+
+package org.epics.graphene;
+
+import java.awt.*;
+import java.awt.geom.Path2D;
+import java.util.Arrays;
+import org.epics.util.array.ListNumber;
+
+/**
+ *
+ * @author Samuel
+ */
+public class SparklineGraph2DRenderer extends Graph2DRenderer<Graph2DRendererUpdate>{
+    
+}
