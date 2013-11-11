096286524f0e14c08647c8ce89b1351a7ad60ab3
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-11-11 16:52:51 -0500

    integration: adding enum test

--- pvmanager-integration/src/main/java/org/epics/pvmanager/integration/Main2.java
+++ pvmanager-integration/src/main/java/org/epics/pvmanager/integration/Main2.java
@@ -4,6 +4,7 @@
  */
 package org.epics.pvmanager.integration;
 
+import java.util.Arrays;
 import org.epics.pvmanager.PVManager;
 import org.epics.pvmanager.PVReader;
 import org.epics.pvmanager.jca.JCADataSource;
@@ -15,6 +16,7 @@ import org.epics.util.time.TimeDuration;
 import org.epics.util.time.Timestamp;
 import org.epics.vtype.AlarmSeverity;
 import org.epics.vtype.Time;
+import org.epics.vtype.VEnum;
 import static org.epics.vtype.ValueFactory.*;
 
 /**
@@ -31,6 +33,7 @@ public class Main2 {
             public void run() throws Exception{
                 addReader(PVManager.read(channel("const-double")), TimeDuration.ofHertz(50));
                 addReader(PVManager.read(channel("const-double.NAME")), TimeDuration.ofHertz(50));
+                addReader(PVManager.read(channel("const-double.SCAN")), TimeDuration.ofHertz(50));
                 Thread.sleep(1000);
             }
 
@@ -40,6 +43,8 @@ public class Main2 {
                 log.matchValues("const-double", newVDouble(0.13, newAlarm(AlarmSeverity.INVALID, "UDF_ALARM"), newTime(Timestamp.of(631152000, 0), null, false), displayNone()));
                 log.matchConnections("const-double.NAME", true);
                 log.matchValues("const-double.NAME", newVString("const-double", newAlarm(AlarmSeverity.INVALID, "UDF_ALARM"), newTime(Timestamp.of(631152000, 0), null, false)));
+                log.matchConnections("const-double.SCAN", true);
+                log.matchValues("const-double.SCAN", newVEnum(0, Arrays.asList("Passive", "Event", "I/O Intr", "10 second", "5 second", "2 second", "1 second", ".5 second", ".2 second", ".1 second"), newAlarm(AlarmSeverity.INVALID, "UDF_ALARM"), newTime(Timestamp.of(631152000, 0), null, false)));
             }
         };
         
