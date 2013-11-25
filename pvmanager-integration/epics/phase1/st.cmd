279b065c1df4a2842f17098bc43a8d7fb66a2254
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-11-25 11:11:24 -0500

    integration: adding other pvs based on Michael's test ioc

--- pvmanager-integration/epics/phase1/st.cmd
+++ pvmanager-integration/epics/phase1/st.cmd
@@ -1,3 +1,5 @@
 #dbLoadTemplate test.substitutions
+dbLoadRecords("alarm-change.db", "P=TST:Alarm:,DBL=Double,LONG=Long,STR=String")
+dbLoadRecords("ramp-change.db", "P=TST:Ramp:,DBL=Double,LONG=Long,STR=String,MBBI=Mbbi")
 dbLoadDatabase phase1.db
 iocInit
