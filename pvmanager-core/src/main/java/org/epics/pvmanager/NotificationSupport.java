79465faa498439c4a0a228814934e02a1b7ffdea
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-11-26 15:52:03 -0500

    core: reformat

--- pvmanager-core/src/main/java/org/epics/pvmanager/NotificationSupport.java
+++ pvmanager-core/src/main/java/org/epics/pvmanager/NotificationSupport.java
@@ -8,7 +8,7 @@ import org.epics.pvmanager.util.NullUtils;
 
 /**
  * Dedicated notification type support.
- * 
+ *
  * @param <T> type for which the notifications are prepared
  * @author bknerr
  * @since 17.01.2011
@@ -23,7 +23,7 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
     public NotificationSupport(Class<T> clazz) {
         super(clazz, NotificationSupport.class);
     }
-    
+
     /**
      * Returns the final value by using the appropriate type support.
      *
@@ -35,15 +35,15 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
     public static <T> Notification<T> notification(final T oldValue, final T newValue) {
         @SuppressWarnings("unchecked")
         Class<T> typeClass = (Class<T>) newValue.getClass();
-        NotificationSupport<T> support =
-            (NotificationSupport<T>) findTypeSupportFor(NotificationSupport.class,
-                                                              typeClass);
+        NotificationSupport<T> support
+                = (NotificationSupport<T>) findTypeSupportFor(NotificationSupport.class,
+                        typeClass);
         if (support == null) {
             throw new RuntimeException("Values for type " + typeClass.getSimpleName() + " are not setup for notification.");
         }
         return support.prepareNotification(oldValue, newValue);
-    }    
-    
+    }
+
     /**
      * Given the old and new value, prepare the final value that will be notified.
      * This method is guaranteed to be called in the notification thread (the
@@ -55,7 +55,7 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
      * @return the value to be notified
      */
     public abstract Notification<T> prepareNotification(final T oldValue, final T newValue);
-    
+
     /**
      * Support for notification of immutable types. Notification is enabled if
      * the value changed according to {@link Object#equals(java.lang.Object) }.
@@ -66,13 +66,13 @@ public abstract class NotificationSupport<T> extends TypeSupport<T> {
      */
     public static <T> NotificationSupport<T> immutableTypeSupport(final Class<T> clazz) {
         return new NotificationSupport<T>(clazz) {
-          @Override
-          public Notification<T> prepareNotification(final T oldValue, final T newValue) {
-              if (NullUtils.equalsOrBothNull(oldValue, newValue)) {
-                return new Notification<T>(false, null);
+            @Override
+            public Notification<T> prepareNotification(final T oldValue, final T newValue) {
+                if (NullUtils.equalsOrBothNull(oldValue, newValue)) {
+                    return new Notification<T>(false, null);
+                }
+                return new Notification<T>(true, newValue);
             }
-              return new Notification<T>(true, newValue);
-          }
         };
     }
 }
