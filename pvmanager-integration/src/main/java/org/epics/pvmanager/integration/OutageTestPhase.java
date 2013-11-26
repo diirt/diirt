6be0753d6114972b8aa5f37adb232c5a332ae562
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-11-26 10:03:31 -0500

    integration: testing network outage

---
+++ pvmanager-integration/src/main/java/org/epics/pvmanager/integration/OutageTestPhase.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory All rights reserved. Use
+ * is subject to license terms.
+ */
+package org.epics.pvmanager.integration;
+
+import org.epics.pvmanager.integration.*;
+import static org.epics.pvmanager.ExpressionLanguage.*;
+import org.epics.pvmanager.PVManager;
+import org.epics.util.time.TimeDuration;
+import org.epics.util.time.Timestamp;
+import org.epics.vtype.AlarmSeverity;
+import static org.epics.pvmanager.integration.VTypeMatchMask.*;
+import org.epics.pvmanager.jca.JCADataSource;
+import static org.epics.vtype.ValueFactory.*;
+
+/**
+ * Tests reconnects caused by a server restart.
+ *
+ * @author carcassi
+ */
+public class OutageTestPhase extends TestPhase {
+
+    @Override
+    public void run() throws Exception {
+        // Open command writer
+        addWriter("command", PVManager.write(channel("command")));
+        Thread.sleep(1000);
+
+        // Reset ioc to known state
+        write("command", "start phase1 1");
+        Thread.sleep(10000);
+        
+        // Add all constant fields
+        // TODO: missing float, int, short, byte, string and all arrays
+        addReader(PVManager.read(channel("const-double")), TimeDuration.ofHertz(50));
+
+        // Send restart command and wait enough time
+        write("command", "netpause 35");
+        Thread.sleep(40000);
+    }
+
+    @Override
+    public void verify(Log log) {
+        log.matchConnections("const-double", true, false, true);
+        log.matchValues("const-double", ALL,
+                newVDouble(0.13, newAlarm(AlarmSeverity.INVALID, "UDF_ALARM"), newTime(Timestamp.of(631152000, 0), null, false), displayNone()),
+                newVDouble(0.13, newAlarm(AlarmSeverity.UNDEFINED, "Disconnected"), newTime(Timestamp.of(631152000, 0), null, false), displayNone()),
+                newVDouble(0.13, newAlarm(AlarmSeverity.INVALID, "UDF_ALARM"), newTime(Timestamp.of(631152000, 0), null, false), displayNone()));
+        log.matchErrors("const-double");
+    }
+
+    public static void main(String[] args) {
+        PVManager.setDefaultDataSource(new JCADataSource());
+        TestPhase phase1 = new OutageTestPhase();
+        phase1.execute();
+        PVManager.getDefaultDataSource().close();
+    }
+}
