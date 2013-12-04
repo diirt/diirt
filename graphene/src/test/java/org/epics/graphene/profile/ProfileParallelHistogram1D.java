90cbac6d64e31d133f66582265d527145b98faba
Aaron Barber <asbarder@umich.edu>
2013-12-04 17:41:37 -0500

    Commented profile classes 	-histogram 	-intensity graph 	-line graph 	-scatter graph 	-sparkline graph 	-parallel histogram 	-lock histogram
    ProfileGraph2D
    	-fixes for header and CSV format
    
    ProfileLockHistogram1D
    	-noticed error
    	-removed erroneous code
    	-need to add correct code

--- graphene/src/test/java/org/epics/graphene/profile/ProfileParallelHistogram1D.java
+++ graphene/src/test/java/org/epics/graphene/profile/ProfileParallelHistogram1D.java
@@ -10,14 +10,24 @@ import java.util.concurrent.Executors;
 import org.epics.graphene.*;
 
 /**
+ * Handles profiling for <code>Histogram1D</code> in parallel (by threading).
+ * Takes a <code>Histogram1D</code> dataset and repeatedly renders with several threads of <code>AreaGraph2DRenderer</code> objects.
  * 
  * @author asbarber
  */
 public class ProfileParallelHistogram1D extends ProfileHistogram1D{
     
+    /**
+     * Creates a tool to profile <code>Histogram1D</code> where multiple renders are performed by several threads of histogram profilers.
+     */
     public ProfileParallelHistogram1D(){
         initDatasets();
     }
+    
+    /**
+     * Creates Gaussian random histogram data of the appropriate size.
+     * Updates the circular point data buffer with the histogram data.
+     */    
     private void initDatasets(){
         Point1DDatasetUpdate update = new Point1DDatasetUpdate();
         int maxValue = 1;
@@ -32,7 +42,9 @@ public class ProfileParallelHistogram1D extends ProfileHistogram1D{
         dataset = Histograms.createHistogram(datasetBuffer);              
     }
     
-    //Amount of Threads
+    /**
+     * Number of threads running histogram profilers.
+     */    
     private static int nThreads = 4;
     
     //Dataset of each profiler
@@ -40,17 +52,30 @@ public class ProfileParallelHistogram1D extends ProfileHistogram1D{
     private final Point1DCircularBuffer datasetBuffer = new Point1DCircularBuffer(nSamples);
     private Histogram1D dataset;    
     
-    
+    /**
+     * Gets the histogram data used for each thread.
+     * @see <code>initDatasets()</code>
+     * @return the histogram data to be drawn
+     */    
     @Override
     protected final Histogram1D getDataset() {
       return dataset;
     }
 
+    /**
+     * Returns the name of the graph being profiled.
+     * @return <code>Histogram1D</code> parallel profile title
+     */     
     @Override
     public String getGraphTitle() {
         return "ParallelHistogram1D";
     }
     
+    /**
+     * Profiles for <code>Histogram1D</code> with parallel threads, 
+     * prints the statistics to the console and saves the statistics.
+     * @param args console arguments -- no impact
+     */      
     public static void main(String[] args) {
 
         ExecutorService executor = Executors.newFixedThreadPool(nThreads);
@@ -62,6 +87,7 @@ public class ProfileParallelHistogram1D extends ProfileHistogram1D{
                 public void run() {
                     ProfileParallelHistogram1D profiler = new ProfileParallelHistogram1D();
                     profiler.profile();
+                    profiler.saveStatistics();
                     System.out.println(profiler.getStatistics().toString());
                 }   
                 
