23a9b8578fb3c8180fa43ae5086221febd1c8ab2
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-12-10 10:16:17 -0500

    sample: adding another default to intensity graph sample

--- pvmanager-sample/src/main/java/org/epics/pvmanager/sample/IntensityGraphApp.java
+++ pvmanager-sample/src/main/java/org/epics/pvmanager/sample/IntensityGraphApp.java
@@ -4,17 +4,10 @@
  */
 package org.epics.pvmanager.sample;
 
-import org.epics.graphene.AreaGraph2DRendererUpdate;
 import org.epics.graphene.IntensityGraph2DRendererUpdate;
-import org.epics.graphene.InterpolationScheme;
-import org.epics.graphene.LineGraph2DRendererUpdate;
-import org.epics.pvmanager.graphene.ScatterGraph2DExpression;
 import static org.epics.pvmanager.formula.ExpressionLanguage.formula;
-import org.epics.pvmanager.graphene.AreaGraph2DExpression;
 import static org.epics.pvmanager.graphene.ExpressionLanguage.*;
-import org.epics.pvmanager.graphene.HistogramGraph2DExpression;
 import org.epics.pvmanager.graphene.IntensityGraph2DExpression;
-import org.epics.pvmanager.graphene.LineGraph2DExpression;
 
 /**
  *
@@ -25,6 +18,7 @@ public class IntensityGraphApp extends BaseGraphApp<IntensityGraph2DRendererUpda
     public IntensityGraphApp() {
         dataFormulaField.setModel(new javax.swing.DefaultComboBoxModel<String>(
                 new String[] { "sim://gaussianWaveform",
+                    "sim://sineWaveform(1,50,2000,0.01)",
                     "=arrayWithBoundaries(arrayOf(1,3,2,4,3,5), range(-10,10))",
                     "=caHistogram(\"histo\")"}));
     }
