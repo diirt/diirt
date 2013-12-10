2026290cfb1cdaa9a31b3918139d4bc326b0dcb7
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-12-10 10:48:24 -0500

    integration: fixing headers

--- pvmanager-integration/src/main/java/org/epics/pvmanager/integration/Validators.java
+++ pvmanager-integration/src/main/java/org/epics/pvmanager/integration/Validators.java
@@ -1,9 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (C) 2010-12 Brookhaven National Laboratory
+ * All rights reserved. Use is subject to license terms.
  */
-
 package org.epics.pvmanager.integration;
 
 import java.util.Arrays;
