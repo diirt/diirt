f691dd9bcd796030bb2e70cef72900cf037b941b
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2013-12-18 14:53:05 -0500

    integration: added guard in case softIoc directory does not exist

--- pvmanager-integration/epics/bin/server-script.sh
+++ pvmanager-integration/epics/bin/server-script.sh
@@ -18,12 +18,17 @@ while true; do
      start*) echo Executing \"$COMMAND\"
              NSEC=`echo $COMMAND | cut -d " " -f 3`
              IOC=`echo $COMMAND | cut -d " " -f 2`
-             echo Stopping current IOC
-             ./stop-ioc.sh &> /dev/null
-             echo Waiting $NSEC seconds
-             sleep `echo $COMMAND | cut -d " " -f 3`
-             ./start-ioc.sh `echo $COMMAND | cut -d " " -f 2`
-             sleep 1
+             IOCDIR=`echo ../$IOC`
+             if [ -d "$IOCDIR" ]; then
+               echo Stopping current IOC
+               ./stop-ioc.sh &> /dev/null
+               echo Waiting $NSEC seconds
+               sleep `echo $COMMAND | cut -d " " -f 3`
+               ./start-ioc.sh `echo $COMMAND | cut -d " " -f 2`
+               sleep 1
+             else
+               echo IOC $IOC does not exist: skipping command
+             fi
              ;;
      netpause*) echo Executing \"$COMMAND\"
              ./network-pause.sh $ETH `echo $COMMAND | cut -d " " -f 2`
