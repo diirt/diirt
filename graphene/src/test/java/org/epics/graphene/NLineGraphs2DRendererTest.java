1687d058daaf955806e8fec78d7368d7e585c8c9
Samuel Dallstream <sjdallst@gmail.com>
2014-02-06 21:19:45 -0500

    Added other methods for drawing background and scaling to NLineGraphs2DRenderer.

--- graphene/src/test/java/org/epics/graphene/NLineGraphs2DRendererTest.java
+++ graphene/src/test/java/org/epics/graphene/NLineGraphs2DRendererTest.java
@@ -18,49 +18,49 @@ import org.epics.util.array.*;
  * 
  * @authors asbarber, jkfeng, sjdallst
  */
-//public class NLineGraphs2DRendererTest {
-//    /**
-//     * Tests the functions in SparklineGraph2DRenderer
-//     */
-//    public NLineGraphs2DRendererTest() {
-//    }
-//
-//    private static Point2DDataset largeDataset;
-//    
-//    /**
-//     * Sets up the large dataset used in the tests
-//     * @throws Exception 
-//     */
-//    @BeforeClass
-//    public static void setUpClass() throws Exception {
-//        Random rand = new Random(1);
-//        int nSamples = 100000;
-//        double[] waveform = new double[nSamples];
-//        for (int i = 0; i < nSamples; i++) {
-//            waveform[i] = rand.nextGaussian();
-//        }
-//        largeDataset = org.epics.graphene.Point2DDatasets.lineData(waveform);
-//    }
-//
-//    /**
-//     * Empties the memory used in the large dataset
-//     * @throws Exception 
-//     */
-//    @AfterClass
-//    public static void tearDownClass() throws Exception {
-//        largeDataset = null;
-//    }
-//    
-//    
-//    /**
-//     * Tests case of:
-//     * <ul>
-//     *      <li>Min Value = Last Value</li>
-//     *      <li>There exists more than one min value</li>
-//     * </ul>
-//     * 
-//     * @throws Exception Test fails
-//     */    
+public class NLineGraphs2DRendererTest {
+    /**
+     * Tests the functions in SparklineGraph2DRenderer
+     */
+    public NLineGraphs2DRendererTest() {
+    }
+
+    private static Point2DDataset largeDataset;
+    
+    /**
+     * Sets up the large dataset used in the tests
+     * @throws Exception 
+     */
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Random rand = new Random(1);
+        int nSamples = 100000;
+        double[] waveform = new double[nSamples];
+        for (int i = 0; i < nSamples; i++) {
+            waveform[i] = rand.nextGaussian();
+        }
+        largeDataset = org.epics.graphene.Point2DDatasets.lineData(waveform);
+    }
+
+    /**
+     * Empties the memory used in the large dataset
+     * @throws Exception 
+     */
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        largeDataset = null;
+    }
+    
+    
+    /**
+     * Tests case of:
+     * <ul>
+     *      <li>Min Value = Last Value</li>
+     *      <li>There exists more than one min value</li>
+     * </ul>
+     * 
+     * @throws Exception Test fails
+     */    
 //    @Test
 //    public void EvenSplitTest() throws Exception {
 //        double [][] initialData= new double [10][100]; 
@@ -83,6 +83,30 @@ import org.epics.util.array.*;
 //        //Compares to correct image
 //        ImageAssert.compareImages("nlinegraphs2D.EvenSplit", image);
 //    }
+    
+@Test
+    public void singleGraphTest() throws Exception {
+        double [][] initialData= new double [1][100]; 
+        for(int i = 0; i < 1; i++){
+            for(int j = 0; j < 100; j++){
+                initialData[i][j] = i;
+            }
+        }
+
+        //Creates a sparkline graph
+        List<Point2DDataset> data = new ArrayList<Point2DDataset>();
+        for(int i = 0; i < 1; i++){
+            data.add(Point2DDatasets.lineData(initialData[i]));
+        }
+        BufferedImage image = new BufferedImage(640, 480, BufferedImage.TYPE_3BYTE_BGR);
+        Graphics2D g = (Graphics2D) image.getGraphics();
+        NLineGraphs2DRenderer renderer = new NLineGraphs2DRenderer(640,480);
+        renderer.draw(g, data);
+        
+        //Compares to correct image
+        ImageAssert.compareImages("nlinegraphs2D.singleGraph", image);
+    }
+}
 //    @Test
 //    public void UnevenSplitTest() throws Exception {
 //        double [][] initialData= new double [9][100]; 
