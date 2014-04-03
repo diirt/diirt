a812ffa3c0632645e1adf72cd79aeb1055a79be8
Samuel Dallstream <sjdallst@gmail.com>
2014-04-03 17:54:09 -0400

    Made buildable, deleted valuecolorschemes class.

--- graphene/src/main/java/org/epics/graphene/ValueColorSchemeInstanceOptimizer.java
+++ graphene/src/main/java/org/epics/graphene/ValueColorSchemeInstanceOptimizer.java
@@ -1,6 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (C) 2012-14 graphene developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
 package org.epics.graphene;
 
