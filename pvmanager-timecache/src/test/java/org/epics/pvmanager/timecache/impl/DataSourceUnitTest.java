7c48a8f615a70b989e2e0b719108e8f297bae91f
Frederic ARNAUD <frederic.arnaud@iter.org>
2014-04-10 14:18:37 +0000

    pvmanager-timecache: updated copyrights

--- pvmanager-timecache/src/test/java/org/epics/pvmanager/timecache/impl/DataSourceUnitTest.java
+++ pvmanager-timecache/src/test/java/org/epics/pvmanager/timecache/impl/DataSourceUnitTest.java
@@ -2,13 +2,6 @@
  * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
  * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
-/*******************************************************************************
- * Copyright (c) 2010-2014 ITER Organization.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- ******************************************************************************/
 package org.epics.pvmanager.timecache.impl;
 
 import java.text.DateFormat;
