f6a7313cfd4920f2d3bbee70672221d1f55125bc
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-04-23 17:00:25 -0400

    intensity graph: if next line is the same, just copy the previous

--- graphene/src/main/java/org/epics/graphene/GraphBuffer.java
+++ graphene/src/main/java/org/epics/graphene/GraphBuffer.java
@@ -52,22 +52,35 @@ public class GraphBuffer {
     public void drawDataImage(int xStartPoint, int yStartPoint,
             int[] xPointToDataMap, int[] yPointToDataMap,
             Cell2DDataset data, NumberColorMapInstance colorMap) {
+        int previousYData = -1;
+        int previousXData = -1;
         for (int yOffset = 0; yOffset < yPointToDataMap.length; yOffset++) {
             int yData = yPointToDataMap[yOffset];
-            for (int xOffset = 0; xOffset < xPointToDataMap.length; xOffset++) {
-                int xData = xPointToDataMap[xOffset];
-                int rgb = colorMap.colorFor(data.getValue(xData, yData));
-                if(hasAlphaChannel){
-                    pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 0] = (byte)(rgb >> 24 & 0xFF);
-                    pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 1] = (byte)(rgb & 0xFF);
-                    pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 2] = (byte)(rgb >> 8 & 0xFF);
-                    pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 3] = (byte)(rgb >> 16 & 0xFF);
+            if (yData != previousYData) {
+                for (int xOffset = 0; xOffset < xPointToDataMap.length; xOffset++) {
+                    int xData = xPointToDataMap[xOffset];
+                    int rgb = colorMap.colorFor(data.getValue(xData, yData));
+                    if(hasAlphaChannel){
+                        pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 0] = (byte)(rgb >> 24 & 0xFF);
+                        pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 1] = (byte)(rgb & 0xFF);
+                        pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 2] = (byte)(rgb >> 8 & 0xFF);
+                        pixels[(yStartPoint + yOffset)*width*4 + 4*(xStartPoint + xOffset) + 3] = (byte)(rgb >> 16 & 0xFF);
+                    } else {
+                        pixels[(yStartPoint + yOffset)*width*3 + 3*(xStartPoint + xOffset) + 0] = (byte)(rgb & 0xFF);
+                        pixels[(yStartPoint + yOffset)*width*3 + 3*(xStartPoint + xOffset) + 1] = (byte)((rgb >> 8 & 0xFF) );
+                        pixels[(yStartPoint + yOffset)*width*3 + 3*(xStartPoint + xOffset) + 2] = (byte)((rgb >> 16 & 0xFF));
+                    }
+                }
+            } else {
+                if (hasAlphaChannel) {
+                    System.arraycopy(pixels, (yStartPoint + yOffset - 1)*width*4 + 4*xStartPoint,
+                            pixels, (yStartPoint + yOffset)*width*4 + 4*xStartPoint, xPointToDataMap.length*4);
                 } else {
-                    pixels[(yStartPoint + yOffset)*width*3 + 3*(xStartPoint + xOffset) + 0] = (byte)(rgb & 0xFF);
-                    pixels[(yStartPoint + yOffset)*width*3 + 3*(xStartPoint + xOffset) + 1] = (byte)((rgb >> 8 & 0xFF) );
-                    pixels[(yStartPoint + yOffset)*width*3 + 3*(xStartPoint + xOffset) + 2] = (byte)((rgb >> 16 & 0xFF));
+                    System.arraycopy(pixels, (yStartPoint + yOffset - 1)*width*3 + 3*xStartPoint,
+                            pixels, (yStartPoint + yOffset)*width*3 + 3*xStartPoint, xPointToDataMap.length*3);
                 }
             }
+            previousYData = yData;
         }
     }
 }
