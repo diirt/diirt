85644af6c849d78f9a14dcd56454a04b8ee38a7b
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-05-22 09:32:10 -0400

    formula: hopefully better dynamic range for automatic histogram.

--- pvmanager-extra/src/main/java/org/epics/pvmanager/formula/HistogramOfFormulaFunction.java
+++ pvmanager-extra/src/main/java/org/epics/pvmanager/formula/HistogramOfFormulaFunction.java
@@ -26,12 +26,7 @@ import org.epics.vtype.VNumberArray;
  * @author shroffk
  * 
  */
-class HistogramOfFormulaFunction implements FormulaFunction {
-
-    @Override
-    public boolean isPure() {
-	return true;
-    }
+class HistogramOfFormulaFunction extends StatefulFormulaFunction {
 
     @Override
     public boolean isVarArgs() {
@@ -62,6 +57,11 @@ class HistogramOfFormulaFunction implements FormulaFunction {
     public Class<?> getReturnType() {
 	return VNumber.class;
     }
+    
+    private VNumberArray previousValue;
+    private VNumberArray previousResult;
+    private double previousMaxCount;
+    private Range previousXRange;
 
     @Override
     public Object calculate(List<Object> args) {
@@ -69,15 +69,27 @@ class HistogramOfFormulaFunction implements FormulaFunction {
         if (numberArray == null) {
             return null;
         }
+        
+        // If no change, return previous
+        if (previousValue == numberArray) {
+            return previousResult;
+        }
+        
         Statistics stats = StatisticsUtil.statisticsOf(numberArray.getData());
         int nBins = 100;
+        Range aggregatedRange = Ranges.aggregateRange(stats, previousXRange);
+        Range xRange;
+        if (Ranges.overlap(stats, aggregatedRange) >= 0.9) {
+            xRange = aggregatedRange;
+        } else {
+            xRange = stats;
+        }
 
         IteratorNumber newValues = numberArray.getData().iterator();
-        double minValueRange = stats.getMinimum().doubleValue();
-        double maxValueRange = stats.getMaximum().doubleValue();
+        double minValueRange = xRange.getMinimum().doubleValue();
+        double maxValueRange = xRange.getMaximum().doubleValue();
         
         ListNumber xBoundaries = ListNumbers.linearListFromRange(minValueRange, maxValueRange, nBins + 1);
-        Range xRange = stats;
         String unit = numberArray.getUnits();
         int[] binData = new int[nBins];
         double maxCount = 0;
@@ -98,8 +110,17 @@ class HistogramOfFormulaFunction implements FormulaFunction {
             }
         }
         
-	return newVNumberArray(new ArrayInt(binData), new ArrayInt(nBins), Arrays.asList(newDisplay(xBoundaries, unit)),
+        if (previousMaxCount > maxCount && previousMaxCount < maxCount * 1.1) {
+            maxCount = previousMaxCount;
+        }
+        
+        previousMaxCount = maxCount;
+        previousXRange = xRange;
+        previousValue = numberArray;
+        previousResult = newVNumberArray(new ArrayInt(binData), new ArrayInt(nBins), Arrays.asList(newDisplay(xBoundaries, unit)),
 		numberArray, numberArray, newDisplay(0.0, 0.0, 0.0, "count", NumberFormats.format(0), maxCount, maxCount, maxCount, Double.NaN, Double.NaN));
+        
+        return previousResult;
     }
 
 }
