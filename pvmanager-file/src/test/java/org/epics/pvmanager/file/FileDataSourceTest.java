83b9225ce10887df470fd69ea82941ccc9e9e91d
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-05-28 15:40:57 -0400

    file: update sent if file changes

--- pvmanager-file/src/test/java/org/epics/pvmanager/file/FileDataSourceTest.java
+++ pvmanager-file/src/test/java/org/epics/pvmanager/file/FileDataSourceTest.java
@@ -4,6 +4,10 @@
  */
 package org.epics.pvmanager.file;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import org.epics.pvmanager.test.CountDownPVReaderListener;
 import org.epics.pvmanager.test.MockDataSource;
 import java.util.Arrays;
@@ -16,6 +20,7 @@ import org.epics.pvmanager.expression.Queue;
 import static org.junit.Assert.*;
 import org.junit.*;
 import static org.epics.pvmanager.vtype.ExpressionLanguage.*;
+import org.epics.util.array.ArrayDouble;
 import static org.epics.util.time.TimeDuration.*;
 import org.epics.vtype.VTable;
 import static org.hamcrest.Matchers.*;
@@ -103,6 +108,53 @@ public class FileDataSourceTest {
 //        listener.await(ofMillis(700));
 //        assertThat(listener.getCount(), equalTo(0));
 //        assertThat(pv.getValue(), equalTo((Object) Arrays.asList(2,3,4)));
+    }
+    
+    @Test
+    public void udpateFile() throws Exception {
+        CountDownPVReaderListener listener = new CountDownPVReaderListener(1, PVReaderEvent.VALUE_MASK);
+        File filename = File.createTempFile("file.", ".csv");
+        PrintWriter writer = new PrintWriter(filename);
+        writer.println("Name,Value");
+        writer.println("Andrew,34");
+        writer.println("Bob,12");
+        writer.close();
+        
+        pv = PVManager.read(vType(filename.toURI().getPath())).from(file)
+                .readListener(listener)
+                .maxRate(ofMillis(10));
+        
+        // Wait for value
+        listener.await(ofMillis(700));
+        assertThat(listener.getCount(), equalTo(0));
+        
+        assertThat(pv.getValue(), instanceOf(VTable.class));
+        VTable vTable = (VTable) pv.getValue();
+        assertThat(vTable.getRowCount(), equalTo(2));
+        assertThat(vTable.getColumnCount(), equalTo(2));
+        assertThat(vTable.getColumnName(0), equalTo("Name"));
+        assertThat(vTable.getColumnName(1), equalTo("Value"));
+        assertThat(vTable.getColumnData(0), equalTo((Object) Arrays.asList("Andrew", "Bob")));
+        assertThat(vTable.getColumnData(1), equalTo((Object) new ArrayDouble(34,12)));
+        
+        listener.resetCount(1);
+        
+        writer = new PrintWriter(new BufferedWriter(new FileWriter(filename, true)));
+        writer.println("Charlie,71");
+        writer.close();
+        
+        // Wait for value
+        listener.await(ofMillis(2000));
+        assertThat(listener.getCount(), equalTo(0));
+        
+        assertThat(pv.getValue(), instanceOf(VTable.class));
+        vTable = (VTable) pv.getValue();
+        assertThat(vTable.getRowCount(), equalTo(3));
+        assertThat(vTable.getColumnCount(), equalTo(2));
+        assertThat(vTable.getColumnName(0), equalTo("Name"));
+        assertThat(vTable.getColumnName(1), equalTo("Value"));
+        assertThat(vTable.getColumnData(0), equalTo((Object) Arrays.asList("Andrew", "Bob", "Charlie")));
+        assertThat(vTable.getColumnData(1), equalTo((Object) new ArrayDouble(34,12,71)));
     }
 
 }
