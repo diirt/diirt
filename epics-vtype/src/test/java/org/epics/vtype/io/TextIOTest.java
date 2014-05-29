dcd05c68bc99d49ce6ed2da5af2cd96b3aeee07f
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-05-29 09:29:37 -0400

    vtype: added ability to write to list files.

--- epics-vtype/src/test/java/org/epics/vtype/io/TextIOTest.java
+++ epics-vtype/src/test/java/org/epics/vtype/io/TextIOTest.java
@@ -4,13 +4,16 @@
  */
 package org.epics.vtype.io;
 
+import java.io.FileWriter;
 import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.Arrays;
 import org.epics.util.array.ArrayDouble;
 import org.epics.util.array.ListNumber;
 import org.epics.vtype.VNumberArray;
 import org.epics.vtype.VStringArray;
 import org.epics.vtype.VType;
+import org.epics.vtype.ValueFactory;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -20,8 +23,9 @@ import static org.hamcrest.CoreMatchers.*;
  * @author carcassi
  */
 public class TextIOTest {
+    
     @Test
-    public void importList1() {
+    public void readList1() throws Exception {
         String inputText = "10\n" +
                 "8\n" +
                 "6\n" +
@@ -34,8 +38,9 @@ public class TextIOTest {
         VNumberArray array = (VNumberArray) value;
         assertThat(array.getData(), equalTo((ListNumber) new ArrayDouble(10,8,6,4,2,0)));
     }
+    
     @Test
-    public void importList2() {
+    public void readList2() throws Exception {
         String inputText = "A\n" +
                 "B\n" +
                 "C\n" +
@@ -47,5 +52,32 @@ public class TextIOTest {
         assertThat(value, instanceOf(VStringArray.class));
         VStringArray array = (VStringArray) value;
         assertThat(array.getData(), equalTo(Arrays.asList("A", "B", "C", "D", "E", "F")));
+    }
+    
+    @Test
+    public void writeList1() throws Exception{
+        VType vType = ValueFactory.toVType(new ArrayDouble(10, 8, 6, 4, 2, 0));
+        StringWriter writer = new StringWriter();
+        TextIO.writeList(vType, writer);
+        writer.flush();
+        assertThat(writer.toString(), equalTo("10.0" + System.lineSeparator() +
+                "8.0" + System.lineSeparator() +
+                "6.0" + System.lineSeparator() +
+                "4.0" + System.lineSeparator() +
+                "2.0" + System.lineSeparator() +
+                "0.0" + System.lineSeparator()));
+    }
+    
+    @Test
+    public void writeList2() throws Exception{
+        VType vType = ValueFactory.toVType(Arrays.asList("A", "B", "C", "D", "E"));
+        StringWriter writer = new StringWriter();
+        TextIO.writeList(vType, writer);
+        writer.flush();
+        assertThat(writer.toString(), equalTo("A" + System.lineSeparator() +
+                "B" + System.lineSeparator() +
+                "C" + System.lineSeparator() +
+                "D" + System.lineSeparator() +
+                "E" + System.lineSeparator()));
     }
 }
