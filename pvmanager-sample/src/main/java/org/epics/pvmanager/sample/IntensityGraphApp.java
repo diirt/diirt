0ddde5b8e0099c4978d7e84330f62e743cfafd1e
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-06-11 14:18:23 -0400

    sample: adding example for creating an ndarray through formula

--- pvmanager-sample/src/main/java/org/epics/pvmanager/sample/IntensityGraphApp.java
+++ pvmanager-sample/src/main/java/org/epics/pvmanager/sample/IntensityGraphApp.java
@@ -23,7 +23,7 @@ public class IntensityGraphApp extends BaseGraphApp<IntensityGraph2DRendererUpda
         dataFormulaField.setModel(new javax.swing.DefaultComboBoxModel<>(
                 new String[] { "sim://gaussianWaveform",
                     "sim://sine2DWaveform(1,50,45,100,100,0.1)",
-                    "=arrayWithBoundaries(arrayOf(1,3,2,4,3,5), range(-10,10))",
+                    "=ndArray(arrayOf(1,3,2,4,3,5), dimDisplay(3,FALSE), dimDisplay(2,FALSE))",
                     "sim://square2DWaveform(1,50,45,10000,10000,0.1)"}));
     }
     
