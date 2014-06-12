977610da52cadd71719496c2fddb3386b2da561f
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-06-12 16:03:03 -0400

    sample: adding 2d histogram example

--- pvmanager-sample/src/main/java/org/epics/pvmanager/sample/IntensityGraphApp.java
+++ pvmanager-sample/src/main/java/org/epics/pvmanager/sample/IntensityGraphApp.java
@@ -25,6 +25,7 @@ public class IntensityGraphApp extends BaseGraphApp<IntensityGraph2DRendererUpda
                     "sim://sine2DWaveform(1,50,45,100,100,0.1)",
                     "=ndArray(arrayOf(1,3,2,4,3,5), dimDisplay(3,FALSE), dimDisplay(2,FALSE))",
                     "=ndArray('13SIM1:image1:ArrayData', dimDisplay('13SIM1:cam1:ArraySizeY_RBV', TRUE), dimDisplay('13SIM1:cam1:ArraySizeX_RBV', FALSE))",
+                    "=histogram2DOf(tableOf(column(\"X\", 'sim://sineWaveform(1,100,100,0.01)'), column(\"Y\", 'sim://sineWaveform(10,100,100,0.01)')), \"Y\", \"X\")",
                     "sim://square2DWaveform(1,50,45,10000,10000,0.1)"}));
     }
     
