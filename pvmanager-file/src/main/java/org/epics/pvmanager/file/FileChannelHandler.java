75a52fd2dea60181345025a72fbbbd9cce9a7a88
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-07-02 09:22:25 -0400

    file: removing unused imports

--- pvmanager-file/src/main/java/org/epics/pvmanager/file/FileChannelHandler.java
+++ pvmanager-file/src/main/java/org/epics/pvmanager/file/FileChannelHandler.java
@@ -7,13 +7,10 @@ package org.epics.pvmanager.file;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.OutputStream;
 
 import org.epics.pvmanager.ChannelWriteCallback;
 import org.epics.pvmanager.MultiplexedChannelHandler;
-import org.epics.vtype.VTable;
-import org.epics.vtype.io.CSVIO;
 
 /**
  * Implementation for channels of a {@link LocalDataSource}.
