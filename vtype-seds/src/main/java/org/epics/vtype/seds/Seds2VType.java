2de82a7e681a7345a7fa97ae9fb42309474d8acc
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-07-11 10:22:38 -0400

    seds: adding skeleton for vtypes<->seds

---
+++ vtype-seds/src/main/java/org/epics/vtype/seds/Seds2VType.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+
+package org.epics.vtype.seds;
+
+import javax.json.JsonObject;
+import org.epics.vtype.VType;
+
+/**
+ * 
+ * @author carcassi
+ */
+public class Seds2VType {
+
+    public static VType toVType(JsonObject json) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+    
+    public static JsonObject toJson(VType vType) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
---
+++ vtype-seds/src/test/java/org/epics/vtype/seds/Seds2VTypeTest.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+
+package org.epics.vtype.seds;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author carcassi
+ */
+public class Seds2VTypeTest {
+
+    @Test
+    public void testVDouble() {
+    }
+    
+}
