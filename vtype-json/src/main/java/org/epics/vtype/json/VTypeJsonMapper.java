83b7a7d35c813bd46d5141bde07ddcef6ae6c73f
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-09-22 10:37:12 -0400

    vtype-json: factored out JSON array convertions

--- vtype-json/src/main/java/org/epics/vtype/json/VTypeJsonMapper.java
+++ vtype-json/src/main/java/org/epics/vtype/json/VTypeJsonMapper.java
@@ -17,12 +17,6 @@ import javax.json.JsonObject;
 import javax.json.JsonString;
 import javax.json.JsonValue;
 import org.epics.util.array.ArrayBoolean;
-import org.epics.util.array.ArrayByte;
-import org.epics.util.array.ArrayDouble;
-import org.epics.util.array.ArrayFloat;
-import org.epics.util.array.ArrayInt;
-import org.epics.util.array.ArrayLong;
-import org.epics.util.array.ArrayShort;
 import org.epics.util.array.ListBoolean;
 import org.epics.util.array.ListByte;
 import org.epics.util.array.ListDouble;
@@ -36,7 +30,7 @@ import org.epics.vtype.AlarmSeverity;
 import org.epics.vtype.Display;
 import org.epics.vtype.Time;
 import org.epics.vtype.ValueFactory;
-
+import static org.epics.vtype.json.JsonArrays.*;
 /**
  *
  * @author carcassi
@@ -94,86 +88,36 @@ class VTypeJsonMapper implements JsonObject {
         return toListDouble(array);
     }
     
-    private ListDouble toListDouble(JsonArray array) {
-        double[] values = new double[array.size()];
-        for (int i = 0; i < values.length; i++) {
-            if (array.isNull(i)) {
-                values[i] = Double.NaN;
-            } else {
-                values[i] = array.getJsonNumber(i).doubleValue();
-            }
-        }
-        return new ArrayDouble(values);
-    }
     
     public ListFloat getListFloat(String string) {
         JsonArray array = getJsonArray(string);
         return toListFloat(array);
     }
     
-    private ListFloat toListFloat(JsonArray array) {
-        float[] values = new float[array.size()];
-        for (int i = 0; i < values.length; i++) {
-            if (array.isNull(i)) {
-                values[i] = Float.NaN;
-            } else {
-                values[i] = (float) array.getJsonNumber(i).doubleValue();
-            }
-        }
-        return new ArrayFloat(values);
-    }
     
     public ListLong getListLong(String string) {
         JsonArray array = getJsonArray(string);
         return toListLong(array);
     }
     
-    private ListLong toListLong(JsonArray array) {
-        long[] values = new long[array.size()];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = (long) array.getJsonNumber(i).longValue();
-        }
-        return new ArrayLong(values);
-    }
     
     public ListInt getListInt(String string) {
         JsonArray array = getJsonArray(string);
         return toListInt(array);
     }
     
-    private ListInt toListInt(JsonArray array) {
-        int[] values = new int[array.size()];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = (int) array.getJsonNumber(i).intValue();
-        }
-        return new ArrayInt(values);
-    }
     
     public ListShort getListShort(String string) {
         JsonArray array = getJsonArray(string);
         return toListShort(array);
     }
     
-    private ListShort toListShort(JsonArray array) {
-        short[] values = new short[array.size()];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = (short) array.getJsonNumber(i).intValue();
-        }
-        return new ArrayShort(values);
-    }
     
     public ListByte getListByte(String string) {
         JsonArray array = getJsonArray(string);
         return toListByte(array);
     }
     
-    private ListByte toListByte(JsonArray array) {
-        byte[] values = new byte[array.size()];
-        for (int i = 0; i < values.length; i++) {
-            values[i] = (byte) array.getJsonNumber(i).intValue();
-        }
-        return new ArrayByte(values);
-    }
 
     public ListBoolean getListBoolean(String string) {
         JsonArray array = getJsonArray(string);
@@ -189,13 +133,6 @@ class VTypeJsonMapper implements JsonObject {
         return toListString(array);
     }
     
-    private List<String> toListString(JsonArray array) {
-        List<String> strings = new ArrayList<>();
-        for (int i = 0; i < array.size(); i++) {
-            strings.add(array.getString(i));
-        }
-        return strings;
-    }
 
     public List<Class<?>> getColumnTypes(String string) {
         JsonArray array = getJsonArray(string);
