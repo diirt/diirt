778be1459404df6cf5b83e689b52434da1dd5773
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-09-24 10:06:48 -0400

    Fixing site-publish.sh

--- site-publish.sh
+++ site-publish.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 cd target
+rm -rf pvmanager-pages
 git clone git@github.com:diirt/pvmanager-pages.git
 cd pvmanager-pages
 rm -rf *
-cp -R ../site .
-git commit --author="Gabriele Carcassi <gabriele.carcassi@gmail.com>" -m "Website update" .
+cp -R ../site/* .
+git add -A
+git commit --author="Gabriele Carcassi <gabriele.carcassi@gmail.com>" -m "Website update"
 git push origin
