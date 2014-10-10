771a1b62d98a76a1e0b78e1497d9761bb061a472
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-10 15:37:55 -0400

    web-pods: fixing package name

--- web-pods/src/main/java/org/diirt/pods/web/WSEndpoint.java
+++ web-pods/src/main/java/org/diirt/pods/web/WSEndpoint.java
@@ -4,18 +4,18 @@
  */
 package org.diirt.pods.web;
 
-import org.diirt.web.pods.common.MessageValueEvent;
-import org.diirt.web.pods.common.MessageConnectionEvent;
-import org.diirt.web.pods.common.Message;
-import org.diirt.web.pods.common.MessageWriteCompletedEvent;
-import org.diirt.web.pods.common.MessageWrite;
-import org.diirt.web.pods.common.MessageSubscribe;
-import org.diirt.web.pods.common.MessageDecoder;
-import org.diirt.web.pods.common.MessageErrorEvent;
-import org.diirt.web.pods.common.MessageUnsubscribe;
-import org.diirt.web.pods.common.MessageEncoder;
-import org.diirt.web.pods.common.MessageResume;
-import org.diirt.web.pods.common.MessagePause;
+import org.diirt.pods.web.common.MessageValueEvent;
+import org.diirt.pods.web.common.MessageConnectionEvent;
+import org.diirt.pods.web.common.Message;
+import org.diirt.pods.web.common.MessageWriteCompletedEvent;
+import org.diirt.pods.web.common.MessageWrite;
+import org.diirt.pods.web.common.MessageSubscribe;
+import org.diirt.pods.web.common.MessageDecoder;
+import org.diirt.pods.web.common.MessageErrorEvent;
+import org.diirt.pods.web.common.MessageUnsubscribe;
+import org.diirt.pods.web.common.MessageEncoder;
+import org.diirt.pods.web.common.MessageResume;
+import org.diirt.pods.web.common.MessagePause;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
