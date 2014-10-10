c88481021e8f75b97492cb03d0c9c948e9831bf4
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-10 15:58:32 -0400

    web-pods-common: Javadoc update

---
+++ web-pods-common/src/main/java/org/diirt/pods/web/common/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+
+/**
+ * Common elements for WebPods client and server implementation. Contains
+ * the code to marshal and un-marshal messages from Java and JSON.
+ */
+package org.diirt.pods.web.common;
