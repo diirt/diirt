7b9a54bb543679bd651c2a998669d71394181bc5
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-27 16:25:12 -0400

    pvmanager: remove use of deprecated utility

--- pvmanager/pvmanager-core/src/main/java/org/epics/pvmanager/util/NullUtils.java
+++ pvmanager/pvmanager-core/src/main/java/org/epics/pvmanager/util/NullUtils.java
@@ -12,22 +12,6 @@ import java.util.Collection;
  * @author carcassi
  */
 public class NullUtils {
-
-    /**
-     * Returns true if both objects are null or they are equal.
-     *
-     * @param obj1 first object
-     * @param obj2 second object
-     * @return true if equals or both are null
-     * @deprecated use Java7 method java.util.Objects.equals
-     */
-    @Deprecated
-    public static boolean equalsOrBothNull(Object obj1, Object obj2) {
-        if (obj1 == null)
-            return obj2 == null;
-        else
-            return obj1.equals(obj2);
-    }
     
     /**
      * Checks whether the collection contains a null value.
