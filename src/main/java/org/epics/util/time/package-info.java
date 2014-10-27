24aca0a173a635aaf0b78d213a3fee8d4f91c077
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-27 10:02:31 -0400

    Javadoc update

--- src/main/java/org/epics/util/time/package-info.java
+++ src/main/java/org/epics/util/time/package-info.java
@@ -5,11 +5,9 @@
 /**
  * Contains basic common classes to handle time at nanosecond precision.
  * <h3>JSR 310 compatibility</h3>
- * Java 8 will introduce a better time definition that is going to be very
- * similar to these class. That effort is unfortunately too unstable to use
- * directly. When it will be released, the plan is to phase out this package
- * and use the standard where possible. Same definitions and conventions
- * are taken from JSR 310 to make future conversion easier.
+ * Java 8 has introduced a better time definition that is going to be very
+ * similar to these class. The plan is to phase out this package
+ * and use the standard where possible.
  * 
  */
 package org.epics.util.time;
