6efd034cd4b4d31f16cf8aed1a46523e75693f8e
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-29 16:08:12 -0400

    sample: showcase for datasource configuration log

---
+++ pvmanager/pvmanager-sample/src/main/java/org/diirt/datasource/sample/log/DataSourceConfigurationLog.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+package org.diirt.datasource.sample.log;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.diirt.datasource.CompositeDataSource;
+import org.diirt.datasource.DataSourceProvider;
+import org.diirt.datasource.PVManager;
+import org.diirt.datasource.PVReader;
+import org.diirt.datasource.PVReaderEvent;
+import org.diirt.datasource.PVReaderListener;
+import static org.diirt.datasource.vtype.ExpressionLanguage.vNumber;
+import org.diirt.util.time.TimeDuration;
+import org.diirt.vtype.VNumber;
+
+/**
+ *
+ * @author carcassi
+ */
+public class DataSourceConfigurationLog {
+    public static void main(String[] args) throws Exception {
+        // Increasing logging at CONFIG level
+        Logger log1 = Logger.getLogger(CompositeDataSource.class.getName());
+        log1.setLevel(Level.CONFIG);
+        Logger log2 = Logger.getLogger(DataSourceProvider.class.getName());
+        log2.setLevel(Level.CONFIG);
+        Logger.getLogger("").getHandlers()[0].setLevel(Level.CONFIG);
+        
+        System.out.println("Open channel");
+        PVReader<VNumber> reader = PVManager.read(vNumber("sim://noise"))
+                .readListener(new PVReaderListener<VNumber>() {
+                    @Override
+                    public void pvChanged(PVReaderEvent<VNumber> event) {
+                        if (event.isValueChanged()) {
+                            System.out.println("... value is " + event.getPvReader().getValue().getValue());
+                        }
+                    }
+                })
+                .maxRate(TimeDuration.ofMillis(500));
+	
+	Thread.sleep(2000);
+        System.out.println("Close channel");
+        reader.close();
+	Thread.sleep(1000);
+	
+        System.out.println("Close data source");
+        PVManager.getDefaultDataSource().close();
+        System.out.println("Done");
+    }
+}
---
+++ pvmanager/pvmanager-sample/src/main/java/org/diirt/datasource/sample/log/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+
+/**
+ * This class contains examples that shows how the logging works.
+ */
+package org.diirt.datasource.sample.log;
