162ae57c0c1e2b470e802844da3332465a56f978
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-10-31 16:45:49 -0400

    pvmanager: javadoc update

--- pvmanager/pvmanager-core/src/main/java/org/diirt/service/Service.java
+++ pvmanager/pvmanager-core/src/main/java/org/diirt/service/Service.java
@@ -10,6 +10,13 @@ import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
+ * Provides access to command/response type of communication for sources of
+ * data or RPC-like services. Each service is a collection of one of more methods.
+ * Each method can be executed with a set of parameters, and returns with a set
+ * of results.
+ * <p>
+ * This class is immutable and therefore thread-safe. Subclasses are allowed
+ * and should also be immutable.
  *
  * @author carcassi
  */
@@ -20,20 +27,41 @@ public class Service {
     private final String description;
     private final Map<String, ServiceMethod> serviceMethods;
 
+    /**
+     * Creates a new service given the description.
+     * 
+     * @param serviceDescription the service description
+     */
     public Service(ServiceDescription serviceDescription) {
         this.name = serviceDescription.name;
         this.description = serviceDescription.description;
         this.serviceMethods = Collections.unmodifiableMap(new HashMap<>(serviceDescription.serviceMethods));
     }
 
+    /**
+     * Returns a brief name for the service. Used for registering and looking the
+     * service up.
+     * 
+     * @return the service name, can't be null
+     */
     public final String getName() {
         return name;
     }
 
+    /**
+     * Returns a description for the service.
+     * 
+     * @return the service description, can't be null
+     */
     public final String getDescription() {
         return description;
     }
 
+    /**
+     * Returns all the methods associated with this service.
+     * 
+     * @return the service method, can't be null
+     */
     public final Map<String, ServiceMethod> getServiceMethods() {
         return serviceMethods;
     }
