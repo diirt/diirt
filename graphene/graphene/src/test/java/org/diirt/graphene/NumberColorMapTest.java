de9b100a07c721239264097f97c8e2bbeab12721
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-11-10 09:33:57 -0500

    graphene: align header with the rest of the project

--- graphene/graphene/src/test/java/org/diirt/graphene/NumberColorMapTest.java
+++ graphene/graphene/src/test/java/org/diirt/graphene/NumberColorMapTest.java
@@ -1,8 +1,7 @@
 /**
- * Copyright (C) 2012-14 graphene developers. See COPYRIGHT.TXT
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
  * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
-
 package org.diirt.graphene;
 
 import org.diirt.graphene.NumberColorMapInstance;
