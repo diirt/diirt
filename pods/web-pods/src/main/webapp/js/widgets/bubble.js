e5cf297cf3b15b0258035a684684763834226c0c
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-11-18 14:11:30 -0500

    web-pods: starting to add column selection to bubble graph

--- pods/web-pods/src/main/webapp/js/widgets/bubble.js
+++ pods/web-pods/src/main/webapp/js/widgets/bubble.js
@@ -6,18 +6,68 @@ function drawSeriesChart() {
     var len = nodes.length;
     var charts = {};
     for (var i = 0; i < len; i++) {
-        var k = 1;
+        // Extract the node and all its properties
+        var masterDiv = nodes[i];
+        var id = nodes[i].getAttribute("id");
         var channelname = nodes[i].getAttribute("data-channel");
         var xColumn = nodes[i].getAttribute("data-x-column");
         var yColumn = nodes[i].getAttribute("data-y-column");
         var colorColumn = nodes[i].getAttribute("data-color-column");
         var readOnly = true;
+        
+        // Prepare html
+        var tableDiv = document.createElement("div");
+        tableDiv.style.display = "table";
+        tableDiv.style.height = "100%";
+        tableDiv.style.width = "100%";
+        masterDiv.appendChild(tableDiv);
+        
+        var selectX = document.createElement("select");
+        selectX.id = id + "-select-x";
+        var option = document.createElement("option");
+        option.text = "Kiwi";
+//        selectX.add(option);
+//        selectX.style.display = "table-row";
+        tableDiv.appendChild(selectX);
+
+        var graphDiv = document.createElement("div");
+        graphDiv.style.display = "table-row";
+        graphDiv.style.height = "100%";
+        tableDiv.appendChild(graphDiv);
+        
+        // Helper functions
+        var populateSelect = function (select, list) {
+            var currentSelection = null;
+            if (select.selectedIndex !== -1) {
+                currentSelection = select.options[select.selectedIndex].text;
+            }
+            for (i = 0; i < list.length; i++) {
+                if (i < select.options.length) {
+                    select.options[i].text = list[i];
+                } else {
+                    var option = document.createElement("option");
+                    option.text = list[i];
+                    select.add(option, select[i]);
+                }
+                if (select[i].text === currentSelection) {
+                    select.selectedIndex = i;
+                }
+            }
+            if (select.options.length > list.length) {
+                for (i = select.options.length - 1; i >= list.length; i--) {
+                    select.remove(i);
+                }
+            }
+        }
+
+        // Connect to live data
         var callback = function (evt, channel) {
             switch (evt.type) {
                 case "connection": //connection state changed
                     channel.readOnly = !evt.writeConnected;
                     break;
                 case "value": //value changed
+                    populateSelect(selectX, evt.value.columnNames);
                     var xId = evt.value.columnNames.indexOf(xColumn);
                     var yId = evt.value.columnNames.indexOf(yColumn);
                     var colorId = evt.value.columnNames.indexOf(colorColumn);
@@ -50,8 +100,6 @@ function drawSeriesChart() {
             }
         };
         var channel = wp.subscribeChannel(channelname, callback, readOnly);
-        var id = nodes[i].getAttribute("id");
-
 
         var data = google.visualization.arrayToDataTable([
             ['ID', 'X', 'Y', 'Color', 'Size'],
@@ -64,8 +112,10 @@ function drawSeriesChart() {
             vAxis: {title: ''},
             bubble: {textStyle: {fontSize: 11}}
         };
+        
+        populateSelect(selectX, [xColumn]);
 
-        var chart = new google.visualization.BubbleChart(document.getElementById(id));
+        var chart = new google.visualization.BubbleChart(graphDiv);
         chart.draw(data, options);
 
         charts[channel.getId()] = chart;
