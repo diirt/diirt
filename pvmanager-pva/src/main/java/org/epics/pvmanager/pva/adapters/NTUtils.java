acfdac0faa4f982ab0a90521b720c7974d14e01a
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-11-18 14:23:21 -0500

    pva: fixing version and copyright notice

--- pvmanager-pva/src/main/java/org/epics/pvmanager/pva/adapters/NTUtils.java
+++ pvmanager-pva/src/main/java/org/epics/pvmanager/pva/adapters/NTUtils.java
@@ -1,3 +1,7 @@
+/**
+ * Copyright (C) 2010-14 pvmanager developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
 package org.epics.pvmanager.pva.adapters;
 
 import java.util.Arrays;
