5ad17a7d198e0718c6cd403cf2076ed874483d85
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-11-21 10:11:29 -0500

    support.cf: Adding service support for channel finder

---
+++ support/cf/src/main/java/org/diirt/support/cf/service/ChannelFinderServiceProvider.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+package org.diirt.support.cf.service;
+
+import java.util.Collection;
+import java.util.Collections;
+import org.diirt.service.Service;
+import org.diirt.service.ServiceProvider;
+
+/**
+ * The service factory for channel finder.
+ *
+ * @author carcassi
+ */
+public class ChannelFinderServiceProvider implements ServiceProvider {
+
+    @Override
+    public String getName() {
+        return "cf";
+    }
+
+    @Override
+    public Collection<Service> createServices() {
+        return Collections.singleton(new ChannelFinderService());
+    }
+
+}
