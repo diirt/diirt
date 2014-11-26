d80940d6c524f288621c117fd100a1a6c37dbcbc
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2014-11-26 10:17:20 -0500

    sample: fixing headers

--- pvmanager/pvmanager-sample/src/main/java/org/diirt/datasource/sample/MonitorChannel.java
+++ pvmanager/pvmanager-sample/src/main/java/org/diirt/datasource/sample/MonitorChannel.java
@@ -1,6 +1,6 @@
 /**
- * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT All rights
- * reserved. Use is subject to license terms. See LICENSE.TXT
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
 package org.diirt.datasource.sample;
 
