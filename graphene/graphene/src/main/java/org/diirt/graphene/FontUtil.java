03c799c602aa57aa39dc7ce1143a0c849bc9a5ca
Richard Fearn <richard.fearn@esss.se>
2014-12-02 10:39:09 +0100

    graphene: fix a couple of issues in FontUtil
    1. FontUtil had several references to HorizontalAxisRenderer - FontUtil is
       more appropriate
    
    2. The 'name' parameter to loadFont was unused
    
    Also add a minimal unit test for this class.

--- graphene/graphene/src/main/java/org/diirt/graphene/FontUtil.java
+++ graphene/graphene/src/main/java/org/diirt/graphene/FontUtil.java
@@ -20,12 +20,12 @@ public class FontUtil {
     
     private static Font loadFont(String name) {
         try {
-            Font font = Font.createFont(Font.TRUETYPE_FONT, HorizontalAxisRenderer.class.getResourceAsStream("LiberationSans-Regular.ttf"));
+            Font font = Font.createFont(Font.TRUETYPE_FONT, FontUtil.class.getResourceAsStream(name));
             return font.deriveFont(Font.PLAIN, 10);
         } catch (FontFormatException ex) {
-            Logger.getLogger(HorizontalAxisRenderer.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(FontUtil.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
-            Logger.getLogger(HorizontalAxisRenderer.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(FontUtil.class.getName()).log(Level.SEVERE, null, ex);
         }
         throw new RuntimeException("Couldn't load");
     }
