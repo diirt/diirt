f0d30b76df17adad38dd257280309b8b0211efb6
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-01-09 09:26:03 -0500

    tools: playing around with graphene and VImage

---
+++ javafx/tools/src/main/java/org/diirt/ui/tools/VImageView.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+package org.diirt.ui.tools;
+
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.BorderPane;
+import org.diirt.vtype.VImage;
+import org.diirt.vtype.ValueUtil;
+
+public final class VImageView extends BorderPane {
+    
+    private ImageView imageView;
+
+    public VImageView() {
+        imageView = new ImageView();
+        imageView.fitHeightProperty().bind(heightProperty());
+        imageView.fitWidthProperty().bind(widthProperty());
+        setCenter(imageView);
+    }
+    
+    public void setVImage(VImage image) {
+        Image newImage = SwingFXUtils.toFXImage(ValueUtil.toImage(image), (WritableImage) imageView.getImage());
+        imageView.setImage(newImage);
+    }
+
+}
