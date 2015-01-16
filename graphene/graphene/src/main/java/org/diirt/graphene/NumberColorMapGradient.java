6f611732c97dee0dc9a266f114a41bfb4dd04622
Yifeng Yang <yang-yifeng@hotmail.com>
2015-01-16 13:00:51 -0500

    Changed colorFor()

--- graphene/graphene/src/main/java/org/diirt/graphene/NumberColorMapGradient.java
+++ graphene/graphene/src/main/java/org/diirt/graphene/NumberColorMapGradient.java
@@ -18,6 +18,9 @@ import org.diirt.util.array.ListDouble;
 class NumberColorMapGradient implements NumberColorMap {
 
     private final List<Color> colors; 
+    private final List<Double> positions; 
+    private boolean relative; 
+    Color nanColor,belowRange,overRange; 
     private final String name;
 
     /**
@@ -27,29 +30,37 @@ class NumberColorMapGradient implements NumberColorMap {
      * 
      * @param colors 
      */
-    public NumberColorMapGradient(List<Color> colors,ListDouble positions, String name) {
+    public NumberColorMapGradient(List<Color> colors,List<Double> positions,boolean relative,Color nanColor,Color belowRange
+            ,Color overRange, String name) {
         this.colors = colors;
+        this.relative=relative; 
         this.name = name;
-
+        this.nanColor=nanColor; 
+        this.belowRange=belowRange; 
+        this.overRange=overRange; 
+        this.positions=positions;
     }
 
     @Override
     public NumberColorMapInstance createInstance(Range range) {
-        return new ValueColorSchemeInstanceGradient(colors, range);
+        return new ValueColorSchemeInstanceGradient(colors, range,nanColor,belowRange,overRange,relative);
     }
 
     class ValueColorSchemeInstanceGradient implements NumberColorMapInstance {
 
         protected List<Color> colors;
-        protected List<Double> percentages = new ArrayList<>();
-        protected int nanColor;
+        protected List<Double> positions; 
+        protected int nanColor,belowRange,overRange;
         protected Range range;
-
-        public ValueColorSchemeInstanceGradient(List<Color> colors, Range range) {
+        private boolean relative; 
+        public ValueColorSchemeInstanceGradient(List<Color> colors, Range range,Color nanColor,Color belowRange
+                ,Color overRange,boolean relative) {
             this.range = range;
             this.colors = colors;
-            this.nanColor = colors[colors.length - 1].getRGB();
-            percentages = percentageRange(colors.length - 2);
+            this.nanColor =nanColor.getRGB();
+             this.belowRange=belowRange.getRGB(); 
+            this.overRange=overRange.getRGB(); 
+            this.relative=relative; 
         }
 
         @Override
@@ -62,19 +73,22 @@ class NumberColorMapGradient implements NumberColorMap {
             }
             double fullRange = range.getMaximum() - range.getMinimum();
             int alpha = 0, red = 0, green = 0, blue = 0;
-            if (fullRange > 0) {
-                for (int i = 0; i < percentages.size() - 1; i++) {
-                    if (range.getMinimum() + percentages.get(i) * fullRange <= value && value <= range.getMinimum() + percentages.get(i + 1) * fullRange) {
-                        double normalValue = MathUtil.normalize(value, range.getMinimum() + percentages.get(i) * fullRange, range.getMinimum() + percentages.get(i + 1) * fullRange);
-                        normalValue = Math.min(normalValue, 1.0);
-                        normalValue = Math.max(normalValue, 0.0);
-                        alpha = 255;
-                        red = (int) (colors[i].getRed() + (colors[i + 1].getRed() - colors[i].getRed()) * normalValue);
-                        green = (int) (colors[i].getGreen() + (colors[i + 1].getGreen() - colors[i].getGreen()) * normalValue);
-                        blue = (int) (colors[i].getBlue() + (colors[i + 1].getBlue() - colors[i].getBlue()) * normalValue);
+            if(relative){
+                if (fullRange > 0) {
+                    for (int i = 0; i < positions.size() - 1; i++) {
+                        if (range.getMinimum() + positions.get(i) * fullRange <= value && value <= range.getMinimum() + positions.get(i + 1) * fullRange) {
+                            double normalValue = MathUtil.normalize(value, range.getMinimum() + positions.get(i) * fullRange, range.getMinimum() + positions.get(i + 1) * fullRange);
+                            normalValue = Math.min(normalValue, 1.0);
+                            normalValue = Math.max(normalValue, 0.0);
+                            alpha = 255;
+                            red = (int) (colors.get(i).getRed() + (colors.get(i+1).getRed() - colors.get(i).getRed()) * normalValue);
+                            green = (int) (colors.get(i).getGreen() + (colors.get(i+1).getGreen() - colors.get(i).getGreen()) * normalValue);
+                            blue = (int) (colors.get(i).getBlue() + (colors.get(i+1).getBlue() - colors.get(i).getBlue()) * normalValue);
+                        }
                     }
-                }
-            } else {
+                } 
+            }
+            /*else {
                 for (int i = 0; i < percentages.size() - 1; i++) {
                     if (percentages.get(i) <= .5 && .5 <= percentages.get(i + 1)) {
                         double normalValue = 0;
@@ -87,17 +101,12 @@ class NumberColorMapGradient implements NumberColorMap {
                     }
                 }
             }
+            */
             if (value > range.getMaximum()) {
-                alpha = 255;
-                red = (colors[colors.length - 2].getRed());
-                green = (colors[colors.length - 2].getGreen());
-                blue = (colors[colors.length - 2].getBlue());
+                return overRange; 
             }
             if (value < range.getMinimum()) {
-                alpha = 255;
-                red = (colors[0].getRed());
-                green = (colors[0].getGreen());
-                blue = (colors[0].getBlue());
+                return belowRange; 
             }
             return (alpha << 24) | (red << 16) | (green << 8) | blue;
         }
@@ -109,18 +118,7 @@ class NumberColorMapGradient implements NumberColorMap {
 
     }
     
-    private static ArrayList<Double> percentageRange(int size) {
-        ArrayList<Double> percentages = new ArrayList<>();
-
-        percentages.add(0.0);
-
-        for (int i = 1; i <= size; i++) {
-            percentages.add((double) i / size);
-        }
-
-        return percentages;
-    }
-
+ 
     @Override
     public String toString() {
         return name;
