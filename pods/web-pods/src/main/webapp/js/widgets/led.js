7f1bcc42d046adf26231acab07756f10560db81e
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-02-16 10:56:51 -0500

    webpods: reviewing led

--- pods/web-pods/src/main/webapp/js/widgets/led.js
+++ pods/web-pods/src/main/webapp/js/widgets/led.js
@@ -9,69 +9,66 @@
  ******************************************************************************/
 
 
-$(document).ready(function() {
-	var nodes = document.getElementsByClassName("led");
-	var len = nodes.length;
+$(document).ready(function () {
+
+    var nodes = document.getElementsByClassName("led");
+    var len = nodes.length;
     var leds = {};
-	for ( var i = 0; i < len; i++) {
-        var channelname = nodes[i].getAttribute("data-channel");
-        var readOnly = nodes[i].getAttribute("data-channel-readonly");
-        var type = nodes[i].getAttribute("data-type");
+    var currentValueStyles = {};
+    counter = 0;
+    
+    function changeValue(value, id, cicle) {
+        var currentValueStyle = currentValueStyles[id];
+        if (currentValueStyle) {
+            cicle.classList.remove(currentValueStyle);
+        }
+        currentValueStyle = "value-" + value;
+        currentValueStyles[id] = currentValueStyle;
+        cicle.classList.add(currentValueStyle);
+    }
+    
+    for (var i = 0; i < len; i++) {
+        var dataChannel = nodes[i].getAttribute("data-channel");
         var id = nodes[i].getAttribute("id");
-        nodes[i].innerHTML = '<canvas id="'+id+'">[No canvas support]</canvas>';
-        fitToContainer(nodes[i].firstChild);
-        if (channelname != null && channelname.trim().length > 0) {
-            var callback = function(evt, channel) {
-                               switch (evt.type) {
-                               case "connection": //connection state changed
-                                   channel.readOnly = !evt.writeConnected;
-                                   break;
-                               case "value": //value changed
-                                   var channelValue = channel.getValue();
-                                   if(channelValue.value) {
-                                      leds[channel.getId()].fillStyle = "#00FF00";
-                                   } else {
-                                      leds[channel.getId()].fillStyle = "#ccc";
-                                   }
-                                   leds[channel.getId()].fill();
-                                   break;
-                               case "error": //error happened
-                                   break;
-                               case "writePermission":	// write permission changed.
-                                   break;
-                               case "writeCompleted": // write finished.
-                                   break;
-                               default:
-                                   break;
-                               }
-                          };
-            var channel = wp.subscribeChannel(channelname, callback, readOnly);
-            var canvas = nodes[i].firstChild;
-            var context = canvas.getContext("2d");
-            var centerX = canvas.width / 2;
-            var centerY = canvas.height / 2;
-            var radius = 10;
-            context.beginPath();
-            context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-            context.fillStyle = "#ccc";
-            context.fill();
-            context.lineWidth = 1;
-            context.strokeStyle = "black";
-            context.stroke();
-            leds[channel.getId()] = context;
+        if (id === null) {
+            counter++;
+            id = "text-monitor-" + counter;
+            nodes[i].id = id;
+        }
+        
+        nodes[i].innerHTML = '<svg height="100%" width="100%"><circle class="value-0" cx="50%" cy="50%" r="48%" stroke="black" stroke-width="1" fill="red" /></svg>';
+        var circle = nodes[i].firstChild.firstChild;
+        if (dataChannel != null && dataChannel.trim().length > 0) {
+            var callback = function (evt, channel) {
+                switch (evt.type) {
+                    case "connection": //connection state changed
+                        channel.readOnly = !evt.writeConnected;
+                        break;
+                    case "value": //value changed
+                        var channelValue = channel.getValue();
+                        if (channelValue.value) {
+                            changeValue(1, channel.getId(), leds[channel.getId()]);
+                        } else {
+                            changeValue(0, channel.getId(), leds[channel.getId()]);
+                        }
+                        break;
+                    case "error": //error happened
+                        break;
+                    case "writePermission":	// write permission changed.
+                        break;
+                    case "writeCompleted": // write finished.
+                        break;
+                    default:
+                        break;
+                }
+            };
+            var channel = wp.subscribeChannel(dataChannel, callback, true);
+            leds[channel.getId()] = circle;
         }
     }
 });
 
-
-function fitToContainer(canvas){
-      canvas.style.width='100%';
-      canvas.style.height='100%';
-      canvas.width  = canvas.offsetWidth;
-      canvas.height = canvas.offsetHeight;
-}
-
-window.onbeforeunload = function() {
-	wp.close();
+window.onbeforeunload = function () {
+    wp.close();
 };
 
