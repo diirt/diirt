fdcae382841219f186ec4025b888da0b09d49791
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-02-17 15:13:48 -0500

    formula: adding alarmOf, re-org alarm functions

--- pvmanager/datasource-formula/src/test/java/org/diirt/datasource/formula/StringFunctionSetTest.java
+++ pvmanager/datasource-formula/src/test/java/org/diirt/datasource/formula/StringFunctionSetTest.java
@@ -40,19 +40,4 @@ public class StringFunctionSetTest extends BaseTestForFormula {
                 .highestAlarmReturned()
                 .latestTimeReturned();
     }
-
-    @Test
-    public void highestSeverity() {
-	VString dataA = newVString("a", alarmNone(), timeNow());
-	VString dataB = newVString("b", alarmNone(), timeNow());
-	VString dataC = newVString("c", alarmNone(), timeNow());
-	VString dataD = newVString("d", newAlarm(AlarmSeverity.MAJOR, "Help!"), timeNow());
-
-        VEnum expected1 = newVEnum(0, AlarmSeverity.labels(), alarmNone(), timeNow());
-        VEnum expected2 = newVEnum(2, AlarmSeverity.labels(), alarmNone(), timeNow());
-        
-        FunctionTester.findByName(set, "highestSeverity")
-                .compareReturnValue(expected1, dataA, dataB, dataC)
-                .compareReturnValue(expected2, dataB, dataC, dataD);
-    }
 }
