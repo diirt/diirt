4191ac051535124f7ca5548c2eb6dd45482ffb20
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-03-03 17:08:28 -0500

    formula: moving channel functions to its own set

---
+++ pvmanager/datasource-formula/src/main/java/org/diirt/datasource/formula/ChannelFunctionSet.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+package org.diirt.datasource.formula;
+
+/**
+ * A set of functions to work with channels.
+ * 
+ * @author carcassi
+ * 
+ */
+public class ChannelFunctionSet extends FormulaFunctionSet {
+
+    /**
+     * Creates a new set.
+     */
+    public ChannelFunctionSet() {
+	super(new FormulaFunctionSetDescription("channel",
+		"Functions to work on channels")
+		.addFormulaFunction(new ChannelFormulaFunction())
+		.addFormulaFunction(new ChannelsFormulaFunction())
+		.addFormulaFunction(new PvFormulaFunction())
+		.addFormulaFunction(new PvsFormulaFunction())
+                );
+    }
+
+}
