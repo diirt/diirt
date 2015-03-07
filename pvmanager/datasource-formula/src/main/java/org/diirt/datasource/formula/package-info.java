149115c586c1ad4878a7df0fb33189a14ce75389
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-03-06 19:11:42 -0500

    formula: javadoc update

--- pvmanager/datasource-formula/src/main/java/org/diirt/datasource/formula/package-info.java
+++ pvmanager/datasource-formula/src/main/java/org/diirt/datasource/formula/package-info.java
@@ -3,6 +3,6 @@
  * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  */
 /**
- * Support for formula expressions (<a href="https://github.com/diirt/diirt/wiki/Formula-language">formula syntax</a>).
+ * Support for formula expressions (<a href="https://github.com/diirt/diirt/wiki/Formula-language" target="_top">formula syntax</a>).
  */
 package org.diirt.datasource.formula;
