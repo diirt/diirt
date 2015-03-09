34ca171ebcd80140837f40f8faa5fd3fc65151c4
Danielle Connolly <danielleoconnolly@gmail.com>
2015-03-09 12:16:21 -0400

    New probe displays details for selected event

--- pods/web-pods/src/main/webapp/clientTEST.js
+++ pods/web-pods/src/main/webapp/clientTEST.js
@@ -5,7 +5,7 @@ window.onload = function() {
     var serverField = document.getElementById('server');
     var channelField = document.getElementById('channel');
     var idField = document.getElementById('idNum');
-    var result = document.getElementById('results');
+    var results = document.getElementById('results');
     var details = document.getElementById('details');
     var subscriptionList = document.getElementById('subscriptions');
     var connectBtn = document.getElementById('connect');
@@ -17,6 +17,9 @@ window.onload = function() {
     var clearBtn = document.getElementById('clear');
     
     var socket;
+    var selectedId;
+    
+    var eventDetails = [];
 
     serverField.value = "ws://" + window.location.host + "/web-pods/socket";
     
@@ -25,16 +28,31 @@ window.onload = function() {
         idField.value = 0;
     }
     
-    function callbackTest(event) {
-        console.log('this is the callback test');
-        console.log(event.value.value);
+    function addEventDetails(event) {
+        eventDetails.unshift('<div><pre>' + JSON.stringify(event, null, '     ') + '</pre></div>');
+    }
+    
+    function newMessage(event) {
+        var newEvent = document.createElement('option');
+        results.insertBefore(newEvent, results.childNodes[0]);
+        newEvent.appendChild(document.createTextNode(event.value.value));
+        addEventDetails(event);
+    }
+    
+    function displayDetails(id) {
+        details.innerHTML = eventDetails[id];
     }
     
     subscribeBtn.onclick = function() {
         var channel = channelField.value;
         var id = idField.value;
-        socket.subscribeChannel(channel, callbackTest);
+        socket.subscribeChannel(channel, newMessage);
         idField.value = idField.value + 1;
+    }
+    
+    results.onchange = function() {
+        selectedId = results.selectedIndex;
+        displayDetails(selectedId);
     }
     
     
