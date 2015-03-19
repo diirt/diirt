a23cf3db17958914bf73824466271c206e64de6a
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-03-19 14:25:12 -0400

    service: javadoc update

--- pvmanager/service/src/main/java/org/diirt/service/package-info.java
+++ pvmanager/service/src/main/java/org/diirt/service/package-info.java
@@ -6,20 +6,20 @@
 /**
  * Support for services (<a href="http://en.wikipedia.org/wiki/Request%E2%80%93response">request/response operations</a>).
  * <p>
- * There are three main objects a generic client will use. The {@link ServiceRegistry}
- * is used to locate the services and methods. The {@link Service} groups together
- * a set of methods that uses the same resources (i.e. thread pool, database
- * connections, ...). The {@link ServiceMethod} represents a single request/response
- * call, which can be executed both synchronously ({@link ServiceMethod#executeSync(java.util.Map) })
- * or asynchronously ({@link ServiceMethod#executeAsync(java.util.Map, java.util.function.Consumer, java.util.function.Consumer) }).
+ * There are three main objects a generic client will use. The {@link org.diirt.service.ServiceRegistry}
+ * is used to locate the services and methods. The {@link org.diirt.service.Service} groups together
+ * a set of methods that use the same resources (i.e. thread pool, database
+ * connections, ...). The {@link org.diirt.service.ServiceMethod} represents a single request/response
+ * call, which can be executed both synchronously ({@link org.diirt.service.ServiceMethod#executeSync(java.util.Map)})
+ * or asynchronously ({@link org.diirt.service.ServiceMethod#executeAsync(java.util.Map, java.util.function.Consumer, java.util.function.Consumer)}).
  * All objects are self-describing (i.e. have a name and description) and are
  * thread-safe.
  * <p>
- * When implementing a service, {@link ServiceMethodDescription} and
- * {@link ServiceDescription} are used to gather the information, so that
+ * When implementing a service, {@link org.diirt.service.ServiceMethodDescription} and
+ * {@link org.diirt.service.ServiceDescription} are used to gather the information, so that
  * the final objects remain immutable and thread-safe. The actual implementation
- * of the call is provided by a subclass of {@link ServiceMethod}. The
- * {@link ServiceProvider} is used to register the service implementation
+ * of the call is provided by a subclass of {@link org.diirt.service.ServiceMethod}. The
+ * {@link org.diirt.service.ServiceProvider} is used to register the service implementation
  * using the <code>ServiceLoader</code> mechanism.
  */
 package org.diirt.service;
