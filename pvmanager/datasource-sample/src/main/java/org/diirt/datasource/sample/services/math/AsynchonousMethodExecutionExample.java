396344935b4633c11d9e869ddc33d1d854c0f2ac
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-03-24 09:57:37 -0400

    sample: asynch method execution example added

---
+++ pvmanager/datasource-sample/src/main/java/org/diirt/datasource/sample/services/math/AsynchonousMethodExecutionExample.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+package org.diirt.datasource.sample.services.math;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Consumer;
+import org.diirt.service.ServiceMethod;
+import org.diirt.service.ServiceRegistry;
+import org.diirt.vtype.Alarm;
+import org.diirt.vtype.Display;
+import org.diirt.vtype.Time;
+import org.diirt.vtype.VDouble;
+import org.diirt.vtype.VInt;
+import org.diirt.vtype.VNumber;
+import org.diirt.vtype.ValueFactory;
+
+/**
+ * Locates a service method and executes it asynchronously (i.e. uses
+ * callbacks when call is finished).
+ *
+ * @author asbarber
+ */
+public class AsynchonousMethodExecutionExample {
+
+    /**
+     * Sample execution of Math services.
+     *
+     * @param args ignored
+     */
+    public static void main(String[] args) {
+        System.out.println("Locating service method...");
+        ServiceMethod method = ServiceRegistry.getDefault().findServiceMethod("math/add");
+        System.out.println("Service method found: " + method);
+        
+        System.out.println("Preparing arguments...");
+        Map<String, Object> arguments = new HashMap<>();
+        arguments.put("arg1", ValueFactory.newVDouble(1.0));
+        arguments.put("arg2", ValueFactory.newVDouble(2.0));
+        System.out.println("Arguments: " + arguments);
+        
+        System.out.println("Executing service...");
+        method.executeAsync(arguments, result -> {
+            System.out.println("Result: " + result);
+        }, ex -> {
+            ex.printStackTrace();
+        });
+    }
+
+}
