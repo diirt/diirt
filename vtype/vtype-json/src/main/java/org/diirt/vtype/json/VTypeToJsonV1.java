516b07b2b4f7b5f9e550400c7d4805038e447001
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-03-30 14:12:40 -0400

    vtype-json: removing table validation

--- vtype/vtype-json/src/main/java/org/diirt/vtype/json/VTypeToJsonV1.java
+++ vtype/vtype-json/src/main/java/org/diirt/vtype/json/VTypeToJsonV1.java
@@ -262,7 +262,6 @@ class VTypeToJsonV1 {
     }
     
     static JsonObject toJson(VTable vTable) {
-        VTableFactory.validateTable(vTable);
         return new JsonVTypeBuilder()
                 .addType(vTable)
                 .addListString("columnNames", VTableFactory.columnNames(vTable))
