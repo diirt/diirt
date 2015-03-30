998f62846f8c74c714f133de4b4d2fdf68a6d6b4
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-03-30 14:13:19 -0400

    pods-web-common: log encoding exception

--- pods/web-pods-common/src/main/java/org/diirt/pods/web/common/MessageEncoder.java
+++ pods/web-pods-common/src/main/java/org/diirt/pods/web/common/MessageEncoder.java
@@ -6,6 +6,8 @@ package org.diirt.pods.web.common;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.EncodeException;
 import javax.websocket.Encoder;
 import javax.websocket.EndpointConfig;
@@ -16,10 +18,16 @@ import javax.websocket.EndpointConfig;
  * @author carcassi
  */
 public class MessageEncoder implements Encoder.TextStream<Message> {
+    private final static Logger log = Logger.getLogger(MessageEncoder.class.getName());
 
     @Override
     public void encode(Message object, Writer writer) throws EncodeException, IOException {
-        object.toJson(writer);
+        try {
+           object.toJson(writer);
+        } catch (RuntimeException ex) {
+            log.log(Level.SEVERE, "Error while preparing event for subscription " + object.getId(), ex);
+            throw ex;
+        }
     }
 
     @Override
