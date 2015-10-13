dls-controls/pvmanager

First attempt at support for local enum PV.

pvmanager-loc/src/main/java/org/epics/pvmanager/loc/LocalDataSource.java
@@ -5,18 +5,17 @@
 package org.epics.pvmanager.loc;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
+
 import org.epics.pvmanager.ChannelHandler;
 import org.epics.pvmanager.ChannelReadRecipe;
 import org.epics.pvmanager.ChannelWriteRecipe;
 import org.epics.pvmanager.DataSource;
 import org.epics.pvmanager.ReadRecipe;
 import org.epics.pvmanager.WriteRecipe;
-import org.epics.pvmanager.vtype.DataTypeSupport;
 import org.epics.pvmanager.util.FunctionParser;
-import org.epics.util.array.ArrayDouble;
+import org.epics.pvmanager.vtype.DataTypeSupport;
 
 /**
  * Data source for locally written data. Each instance of this
@@ -63,9 +62,9 @@ public class LocalDataSource extends DataSource {
         LocalChannelHandler channel = new LocalChannelHandler(parsedTokens.get(0).toString());
         return channel;
     }
-    
+
     private List<Object> parseName(String channelName) {
-        List<Object> tokens = FunctionParser.parseFunctionWithScalarOrArrayArguments(".+", channelName, CHANNEL_SYNTAX_ERROR_MESSAGE);
+        List<Object> tokens = FunctionParser.parseFunctionAnyParameter(".+", channelName);
         String nameAndType = tokens.get(0).toString();
         String name = nameAndType;
         String type = null;
@@ -77,8 +76,21 @@ public class LocalDataSource extends DataSource {
         List<Object> newTokens = new ArrayList<>();
         newTokens.add(name);
         newTokens.add(type);
+        Object initialValue;
+        if ("VEnum".equals(type)) {
+            List<Object> initialValueList = new ArrayList<>();
+            initialValueList.add(tokens.remove(1));
+            Object labels = FunctionParser.asScalarOrList(tokens.subList(1, tokens.size()));
+            if (!(labels instanceof List<?>)) {
+                throw new RuntimeException("Invalid format for VEnum channel.");
+            }
+            initialValueList.add(labels);
+            initialValue = initialValueList;
+        } else {
+            initialValue = FunctionParser.asScalarOrList(tokens.subList(1, tokens.size()));
+        }
         if (tokens.size() > 1) {
-            newTokens.addAll(tokens.subList(1, tokens.size()));
+            newTokens.add(initialValue);
         }
         return newTokens;
     }

diirt/diirt

pvmanager/datasource-loc/src/main/java/org/diirt/datasource/loc/LocalDataSource.java
@@ -5,18 +5,17 @@
 package org.diirt.datasource.loc;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
+
 import org.diirt.datasource.ChannelHandler;
 import org.diirt.datasource.ChannelReadRecipe;
 import org.diirt.datasource.ChannelWriteRecipe;
 import org.diirt.datasource.DataSource;
 import org.diirt.datasource.ReadRecipe;
 import org.diirt.datasource.WriteRecipe;
-import org.diirt.datasource.vtype.DataTypeSupport;
 import org.diirt.datasource.util.FunctionParser;
-import org.diirt.util.array.ArrayDouble;
+import org.diirt.datasource.vtype.DataTypeSupport;
 
 /**
  * Data source for locally written data. Each instance of this
@@ -63,9 +62,9 @@ public class LocalDataSource extends DataSource {
         LocalChannelHandler channel = new LocalChannelHandler(parsedTokens.get(0).toString());
         return channel;
     }
-    
+
     private List<Object> parseName(String channelName) {
-        List<Object> tokens = FunctionParser.parseFunctionWithScalarOrArrayArguments(".+", channelName, CHANNEL_SYNTAX_ERROR_MESSAGE);
+        List<Object> tokens = FunctionParser.parseFunctionAnyParameter(".+", channelName);
         String nameAndType = tokens.get(0).toString();
         String name = nameAndType;
         String type = null;
@@ -77,8 +76,21 @@ public class LocalDataSource extends DataSource {
         List<Object> newTokens = new ArrayList<>();
         newTokens.add(name);
         newTokens.add(type);
+        Object initialValue;
+        if ("VEnum".equals(type)) {
+            List<Object> initialValueList = new ArrayList<>();
+            initialValueList.add(tokens.remove(1));
+            Object labels = FunctionParser.asScalarOrList(tokens.subList(1, tokens.size()));
+            if (!(labels instanceof List<?>)) {
+                throw new RuntimeException("Invalid format for VEnum channel.");
+            }
+            initialValueList.add(labels);
+            initialValue = initialValueList;
+        } else {
+            initialValue = FunctionParser.asScalarOrList(tokens.subList(1, tokens.size()));
+        }
         if (tokens.size() > 1) {
-            newTokens.addAll(tokens.subList(1, tokens.size()));
+            newTokens.add(initialValue);
         }
         return newTokens;
     }