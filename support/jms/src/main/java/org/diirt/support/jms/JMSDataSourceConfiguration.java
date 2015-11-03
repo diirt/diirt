d357961f7accf2dd7a03396d4542952742e22932
Kunal Shroff <shroffk@bnl.gov>
2015-11-03 11:09:31 -0500

    Support for reading a jms.xml config file for jms datasource
    The configuration file allows you to define the Broker URL 
    Use the jms.default.xml as a reference.
    
    Unit tests added for the datasourceConfiguration

--- support/jms/src/main/java/org/diirt/support/jms/JMSDataSourceConfiguration.java
+++ support/jms/src/main/java/org/diirt/support/jms/JMSDataSourceConfiguration.java
@@ -4,10 +4,16 @@
 package org.diirt.support.jms;
 
 import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import javax.jms.JMSException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
 
 import org.diirt.datasource.DataSourceConfiguration;
+import org.w3c.dom.Document;
 
 /**
  * Configuration for {@link JMSDatasource}
@@ -17,12 +23,34 @@ import org.diirt.datasource.DataSourceConfiguration;
  */
 public class JMSDataSourceConfiguration extends DataSourceConfiguration<JMSDatasource> {
 
-    private static final String BROKER_URL = "tcp://localhost:61616?jms.prefetchPolicy.all=1000";
+    private String brokerUrl = "tcp://localhost:61616?jms.prefetchPolicy.all=1000";
 
     @Override
-    public DataSourceConfiguration read(InputStream stream) {
-        // TODO read a configuration file
-        return new JMSDataSourceConfiguration();
+    public JMSDataSourceConfiguration read(InputStream input) {
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(input);
+
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+            XPath xPath = xpathFactory.newXPath();
+            
+            String ver = xPath.evaluate("/jms/@version", document);
+            if (!ver.equals("1")) {
+                throw new IllegalArgumentException("Unsupported version " + ver);
+            }
+
+            String monitorMask = xPath.evaluate("/jms/dataSourceOptions/@brokerURL", document);
+            if (monitorMask != null && !monitorMask.isEmpty()) {
+                this.brokerUrl = monitorMask;
+            }else{
+                Logger.getLogger(JMSDataSourceConfiguration.class.getName()).log(Level.FINEST, "Couldn't load brokerURL from jms file configuration");
+            }
+        } catch (Exception e) {
+            Logger.getLogger(JMSDataSourceConfiguration.class.getName()).log(Level.FINEST, "Couldn't load file configuration", e);
+            throw new IllegalArgumentException("Couldn't load file configuration", e);
+        }
+        return this;
     }
 
     @Override
@@ -31,7 +59,7 @@ public class JMSDataSourceConfiguration extends DataSourceConfiguration<JMSDatas
     }
 
     public String getBrokerUrl() {
-        return BROKER_URL;
+        return brokerUrl;
     }
 
 }
---
+++ support/jms/src/test/java/org/diirt/support/jms/JMSDataSourceConfigurationTest.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2010-14 diirt developers. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ */
+package org.diirt.support.jms;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+/**
+ *
+ * @author carcassi
+ */
+public class JMSDataSourceConfigurationTest {
+
+    @Test
+    public void readDefaultConfiguration() {
+        JMSDataSourceConfiguration conf = new JMSDataSourceConfiguration().read(this.getClass().getResourceAsStream("jms.default.xml"));
+        assertThat(conf.getBrokerUrl(), equalTo("tcp://localhost:61616"));
+    }
+
+    @Test
+    public void readConfiguration1() {
+        JMSDataSourceConfiguration conf = new JMSDataSourceConfiguration().read(this.getClass().getResourceAsStream("jms.xml"));
+        assertThat(conf.getBrokerUrl(), equalTo("tcp://localhost:61616"));
+    }
+
+}
