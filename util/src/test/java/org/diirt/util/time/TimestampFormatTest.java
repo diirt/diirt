cfbe39351b3715672835bd4d15c9145b5e3c5063
Borut Terpinc <borut.terpinc@cosylab.com>
2017-06-20 11:53:51 +0000

    Time-stamp format pattern can be read from settings.xml
    Changes also from Local to ZonedDateTime as format can also include
    zone. Unit tests changed to reflect this.

--- util/src/test/java/org/diirt/util/time/TimestampFormatTest.java
+++ util/src/test/java/org/diirt/util/time/TimestampFormatTest.java
@@ -4,21 +4,21 @@
  */
 package org.diirt.util.time;
 
-import java.text.ParseException;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.TimeZone;
 
+import org.diirt.util.config.SettingsProvider;
+import org.diirt.util.config.TimeStampFormatter;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 
 /**
  *
@@ -30,13 +30,24 @@ public class TimestampFormatTest {
     }
 
     @Test
+    public void timeStampFormaterTest() {
+
+        String formatPatten = SettingsProvider.getSetting("timeStampFormattingPattern");
+        if (formatPatten == null){
+            formatPatten = "yyyy/MM/dd HH:mm:ss.SSS"; //use default pattern.
+        }
+        ZonedDateTime time = ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
+        String expectedValue = DateTimeFormatter.ofPattern(formatPatten).format(time);
+        String actualValue = TimeStampFormatter.TIMESTAMP_FORMAT.format(time);
+        assertThat(actualValue, equalTo(expectedValue));
+    }
+
+    @Test
     public void formatTimestamp1() {
         // Test with milliseconds
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SS");
         Instant time = Instant.ofEpochSecond(0, 300000000);
-        assertThat(
-                ZonedDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(format),
-                equalTo("1970-01-01T00:00:00.30"));
+        assertThat(ZonedDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(format), equalTo("1970-01-01T00:00:00.30"));
     }
 
     @Test
@@ -44,57 +55,48 @@ public class TimestampFormatTest {
         // Test with nanoseconds
         DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.N");
         Instant time = Instant.ofEpochSecond(0, 30000000);
-        assertThat(
-                ZonedDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(format),
-                equalTo("1970-01-01T00:00:00.30000000"));
+        assertThat(ZonedDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(format), equalTo("1970-01-01T00:00:00.30000000"));
     }
 
     @Test
     public void spacedFormat() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-                "yyyy-MM-dd'T'HH:mm:ss.NNNNNN");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.NNNNNN");
         formatter.withZone(TimeZone.getTimeZone("GMT").toZoneId());
         Instant time = Instant.ofEpochSecond(0, 1);
-        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-                equalTo("1970-01-01T00:00:00.000001"));
+        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter), equalTo("1970-01-01T00:00:00.000001"));
         time = Instant.ofEpochSecond(0, 12);
-        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-                equalTo("1970-01-01T00:00:00.000012"));
+        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter), equalTo("1970-01-01T00:00:00.000012"));
         time = Instant.ofEpochSecond(0, 123);
-        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-                equalTo("1970-01-01T00:00:00.000123"));
+        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter), equalTo("1970-01-01T00:00:00.000123"));
         time = Instant.ofEpochSecond(0, 1234);
-        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-                equalTo("1970-01-01T00:00:00.001234"));
+        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter), equalTo("1970-01-01T00:00:00.001234"));
         time = Instant.ofEpochSecond(0, 12345);
-        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-                equalTo("1970-01-01T00:00:00.012345"));
+        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter), equalTo("1970-01-01T00:00:00.012345"));
         time = Instant.ofEpochSecond(0, 123456);
-        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-                equalTo("1970-01-01T00:00:00.123456"));
-//        time = Instant.ofEpochSecond(0, 1234567);
-//        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-//                equalTo("1970-01-01T00:00:00.1234567"));
-//        time = Instant.ofEpochSecond(0, 12345678);
-//        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
-//                equalTo("1970-01-01T00:00:00.12345678"));
+        assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter), equalTo("1970-01-01T00:00:00.123456"));
+        // time = Instant.ofEpochSecond(0, 1234567);
+        // assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
+        // equalTo("1970-01-01T00:00:00.1234567"));
+        // time = Instant.ofEpochSecond(0, 12345678);
+        // assertThat(LocalDateTime.ofInstant(time, TimeZone.getTimeZone("GMT").toZoneId()).format(formatter),
+        // equalTo("1970-01-01T00:00:00.12345678"));
     }
 
-//    @Test
-//    public void specialCases() {
-//        // Only nanoseconds
-//        DateTimeFormatter format = DateTimeFormatter.ofPattern("NNN");
-//        format.withZone(TimeZone.getTimeZone("GMT").toZoneId());
-//        Instant time = Instant.ofEpochSecond(0, 12345);
-//        assertThat(format.format(time), equalTo("12345"));
-//
-//        // Multiple nanoseconds with different format
-//        // and N as part of escaped text
-//        format = DateTimeFormatter.ofPattern("NNNNN-'''N'''-N");
-//        format.withZone(TimeZone.getTimeZone("GMT").toZoneId());
-//        time = Instant.ofEpochSecond(0, 1);
-//        assertThat(format.format(time), equalTo("00001-'N'-1"));
-//    }
+    // @Test
+    // public void specialCases() {
+    // // Only nanoseconds
+    // DateTimeFormatter format = DateTimeFormatter.ofPattern("NNN");
+    // format.withZone(TimeZone.getTimeZone("GMT").toZoneId());
+    // Instant time = Instant.ofEpochSecond(0, 12345);
+    // assertThat(format.format(time), equalTo("12345"));
+    //
+    // // Multiple nanoseconds with different format
+    // // and N as part of escaped text
+    // format = DateTimeFormatter.ofPattern("NNNNN-'''N'''-N");
+    // format.withZone(TimeZone.getTimeZone("GMT").toZoneId());
+    // time = Instant.ofEpochSecond(0, 1);
+    // assertThat(format.format(time), equalTo("00001-'N'-1"));
+    // }
 
     @Test
     public void parse1() throws Exception {
